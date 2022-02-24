52c9bb0c0fa5f5f0fe62482d1af6c8c45451b29c
Marc Nadon <marc.nadon@noaa.gov>
2022-02-24 10:39:56 -1000

    More updates on size data

--- Scripts/GET_SIZE.R
+++ Scripts/GET_SIZE.R
@@ -1,4 +1,4 @@
-require(data.table); require(ggplot2); require(gridExtra); require(directlabels);require(openxlsx); require(ggpmisc);require(directlabels);
+require(data.table); require(ggplot2); require(gridExtra); require(directlabels);require(openxlsx); require(ggpmisc);require(directlabels);require(dplyr)
 
 # Load diver sizes
 US <- readRDS("Outputs\\readyUVS.rds")
@@ -26,47 +26,55 @@ LH <- select(LH,Species,Lmax)
 D  <- merge(D,LH,by="Species")
 
 # Add region weights
-
 D$RW <- 0.8
 D[Area_B=="Manua"]$RW <- 0.2
 
 # Statistics
-Sp <- "VALO"
-E  <- D[Species==Sp&Length_FL>0]
-table(E$Dataset,E$Year)
+MinN <- 30 # Minimum sample size to do size frequency
+Species.List <- unique(D$Species)
+for(i in 1:length(Species.List)){
+ 
+ Sp <- Species.List[i]
+ E  <- D[Species==Sp&Length_FL>0]
+ 
+ # Filter years with low N
+ NB    <- data.table( table(E$Dataset,E$Year) )
+ NB$V2 <- as.numeric(NB$V2)
+ NB    <- NB[N>=MinN]
+ G     <- merge(E,NB,by.x=c("Dataset","Year"),by.y=c("V1","V2"))
 
-ggplot(data=E[Dataset=="Biosampling"])+geom_histogram(aes(x=Length_FL))+facet_wrap(~Year)
-ggplot(data=E[Dataset=="UVS"])+geom_histogram(aes(x=Length_FL))+facet_wrap(~Year)
-ggplot(data=E[Dataset=="BBS"])+geom_histogram(aes(x=Length_FL))+facet_wrap(~Year)
+ Fld <- "Outputs/Graphs/Size/"
+ ggplot(data=G[Dataset=="Biosampling"])+geom_histogram(aes(x=Length_FL,y=..density..))+facet_wrap(~Year,scales="free_y"); ggsave(paste0(Fld,Sp,"_Freq_","BS",".png"))
+ ggplot(data=G[Dataset=="UVS"])+geom_histogram(aes(x=Length_FL,y=..density..))+facet_wrap(~Year,scales="free_y"); ggsave(paste0(Fld,Sp,"_Freq_","US",".png"))
+ ggplot(data=G[Dataset=="BBS"])+geom_histogram(aes(x=Length_FL,y=..density..))+facet_wrap(~Year,scales="free_y"); ggsave(paste0(Fld,Sp,"_Freq_","BB",".png"))
 
-# Prepare dataset for SS3
-E <- E[Dataset=="Biosampling"]
-BIN_SIZE  <- 3 # in cm
-MAXL      <- ceiling(max(E$Lmax))
+ # Prepare dataset for SS3
+ E <- E[Dataset=="Biosampling"]
+ BIN_SIZE  <- 3 # in cm
+ MAXL      <- ceiling(max(E$Lmax))
 
 # Obtain mean weight and mean length per trip
-BINS      <- seq(0,MAXL,by=BIN_SIZE)
-BINS      <- cbind(BINS,seq(1,28,by=1))
-E         <- E[Length_FL<=MAXL]
+ BINS      <- seq(0,MAXL,by=BIN_SIZE)
+ BINS      <- cbind(BINS,seq(1,28,by=1))
+ E         <- E[Length_FL<=MAXL]
 
 # Add length bin lower ends to dataset
-E$LENGTH_BIN_START <- E$Length_FL-(E$Length_FL%%BIN_SIZE)
+ E$LENGTH_BIN_START <- E$Length_FL-(E$Length_FL%%BIN_SIZE)
 
 # Effective Sample size by year
-SAMPSIZE      <- E[Count==1,list(N=.N),by=list(Year,Area_B,RW)]
-SAMPSIZE$EFFN <- SAMPSIZE$N*SAMPSIZE$RW
-SAMPSIZE      <- SAMPSIZE[,list(EFFN=sum(EFFN)),by=list(Year)]
-plot(SAMPSIZE$Year,SAMPSIZE$EFFN)
+ SAMPSIZE      <- E[Count==1,list(N=.N),by=list(Year,Area_B,RW)]
+ SAMPSIZE$EFFN <- SAMPSIZE$N*SAMPSIZE$RW
+ SAMPSIZE      <- SAMPSIZE[,list(EFFN=sum(EFFN)),by=list(Year)]
+ plot(SAMPSIZE$Year,SAMPSIZE$EFFN)
 
 # Calculate abundance-at-length
-E <- E[Count==1,list(Count=sum(Count)),by=list(Year,LENGTH_BIN_START)]
-E <- E[order(Year,LENGTH_BIN_START)]
+ E <- E[Count==1,list(Count=sum(Count)),by=list(Year,LENGTH_BIN_START)]
+ E <- E[order(Year,LENGTH_BIN_START)]
 
-E <- dcast.data.table(E,Year~LENGTH_BIN_START,value.var="Count",fill=0)
+ E <- dcast.data.table(E,Year~LENGTH_BIN_START,value.var="Count",fill=0)
 
-E <- merge(E,SAMPSIZE,by="Year")
-E <- select(E,Year,EFFN,2:ncol(E))
-
-
+ E <- merge(E,SAMPSIZE,by="Year")
+ E <- select(E,Year,EFFN,2:ncol(E))
+}
 
 
