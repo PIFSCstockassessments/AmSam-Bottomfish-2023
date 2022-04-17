7fddc922c9d7273eb20ecabb257805d307c849f7
Marc Nadon <marc.nadon@noaa.gov>
2022-04-17 08:28:03 -1000

    Update FIND_DUPLICATE_TRIPS.R

--- Scripts/FIND_DUPLICATE_TRIPS.R
+++ Scripts/FIND_DUPLICATE_TRIPS.R
@@ -6,18 +6,18 @@ int3 = read.csv(file="Data/a_bbs_int_flat3.csv", stringsAsFactors = F)
 int4 = read.csv(file="Data/a_bbs_int_flat4.csv", stringsAsFactors = F)
 
 interviews <- data.table( rbind(int1,int2,int3,int4) )
-
-
 interviews <- interviews[,list(N=.N),by=list(INTERVIEW_PK,VESSEL_FK,SAMPLE_DATE,METHOD_FK,NUM_DAYS_FISHED)]
-
-
+interviews <- interviews[METHOD_FK==4|METHOD_FK==5]
 interviews$SAMPLE_DATE = as.Date(interviews$SAMPLE_DATE)
-bl = read.csv("Data/AS_bl_allyears.csv", stringsAsFactors = F)
+interviews$YEAR = year(interviews$SAMPLE_DATE)
+
+bl = data.table(  read.csv("Data/AS_bl_allyears.csv", stringsAsFactors = F) )
 bl$SAMPLE_DATE = as.Date(bl$SAMPLE_DATE)
 bl$YEAR <- year(bl$SAMPLE_DATE)
-
+bl <- bl[METHOD_FK==4|METHOD_FK==5]
 
 duplicate_bl = data.frame()
+problem_int = data.frame()
 multiday = interviews[interviews$NUM_DAYS_FISHED > 1,]
 for(i in 1:nrow(multiday)) {
   entry = multiday[i,]
@@ -30,15 +30,30 @@ for(i in 1:nrow(multiday)) {
   
   if(nrow(bl_entries) > 1) {
     duplicate_bl = rbind(duplicate_bl, bl_entries[-nrow(bl_entries),])
+    problem_int = rbind(problem_int,entry)
   }
 }
 
 
 
-nrow(bl[bl$YEAR>=2000,])
-
 hist(duplicate_bl$YEAR)
 
+a <- data.table( table(bl$YEAR))
+b <- data.table( table(interviews$YEAR))
+c <- data.table( table(multiday$YEAR) )
+d <- data.table( table(problem_int$YEAR) )
+e <- merge(a,b,by="V1",all=T)
+f <- merge(e,c,by="V1",all=T)
+g <- merge(f,d,by="V1",all=T)
+g[is.na(g)] <- 0
+setnames(g,c("YEAR","N_boatlog","N_interviews","N_multi","N_problem"))
+g <- g[YEAR>=2000&YEAR<2020]
 
+g$PERC_INTERVIEWED <- round(g$N_interviews/g$N_boatlog*100,0)
+g$PERC_MULTIDAY    <- round(g$N_multi/g$N_interviews*100,0)
+g$PERC_MULTIPROB   <- round(g$N_problem/g$N_multi*100,0)
+g[is.na(g)|g==Inf] <- 0
 
+write.csv(g,"Trips_breakdown.csv")
 
+table(bl$VESSEL_FK)
