8e57fe15fa371198eb1757466fe540b457ef417c
Marc Nadon <marc.nadon@noaa.gov>
2022-04-22 13:54:42 -1000

    Update 01_BBS_data_prep.R

--- Scripts/01_BBS_data_prep.R
+++ Scripts/01_BBS_data_prep.R
@@ -171,6 +171,7 @@ for (i in 1:length(CATCH_PK.list)){
 # View(bbs_3C[INTERVIEW_PK=="20817184804"])
 #Test <- bbs_3C[,list(EST_LBS=max(EST_LBS)),by=list(YEAR,INTERVIEW_PK,CATCH_PK,SPECIES_FK2,SCIENTIFIC_NAME)]
 #Test <- Test[YEAR<=2015&(SPECIES_FK2=="220"|SPECIES_FK2=="229"),list(EST_LBS=sum(EST_LBS)),by=list(SPECIES_FK2)]
+#Prop.Louti; Test[SPECIES_FK2=="229"]$EST_LBS/sum(Test$EST_LBS)
 
 
 # ----- 4b. Pristipomoides filamentosus and P. flavipinnis were confused between 2010-2015. Assume 2016-2021 species is reliable.
@@ -202,6 +203,8 @@ for (i in 1:length(CATCH_PK.list)){
 # View(bbs_3C[INTERVIEW_PK=="100603101305"])
 # Test <- bbs_3C[,list(EST_LBS=max(EST_LBS)),by=list(YEAR,INTERVIEW_PK,CATCH_PK,SPECIES_FK2,SCIENTIFIC_NAME)]
 # Test <- Test[(YEAR>=2010&YEAR<=2015)&(SPECIES_FK2=="241"|SPECIES_FK2=="242"),list(EST_LBS=sum(EST_LBS)),by=list(SPECIES_FK2)]
+# Prop.Flavi; Test[SPECIES_FK2=="241"]$EST_LBS/sum(Test$EST_LBS)
+ 
 
 # ----- 4c. Lethrinidae in Manu'a:
 #   Manu'a fishermen said they catch the red-ear emperor (L. rubrioperculatus) all the time ("100% of trips")
@@ -217,249 +220,41 @@ for (i in 1:length(CATCH_PK.list)){
 
 # calculate proportion of P. filamentosus vs P. flavipinnis for Years > 2015
 
-Prop.Emp    <- bbs_3C[AREA_B=="Manua",list(EST_LBS=max(EST_LBS)),by=list(YEAR,INTERVIEW_PK,CATCH_PK,SPECIES_FK,SCIENTIFIC_NAME)]
-Prop.Emp    <- Prop.Emp[YEAR>2015&(SPECIES_FK=="241"|SPECIES_FK=="242"),list(EST_LBS=sum(EST_LBS)),by=list(SPECIES_FK,SCIENTIFIC_NAME)]
-Prop.Emp    <- Prop.Emp[SPECIES_FK=="241"]$EST_LBS/(Prop.Pristi[SPECIES_FK=="241"]$EST_LBS+Prop.Pristi[SPECIES_FK=="242"]$EST_LBS)
-Prop.Emp    <- round(Prop.Emp,3)
+Prop.Emp    <- bbs_3C[AREA_B=="Manua",list(EST_LBS=max(EST_LBS)),by=list(YEAR,INTERVIEW_PK,CATCH_PK,FAMILY,SPECIES_FK,SCIENTIFIC_NAME)]
+Prop.Emp    <- Prop.Emp[FAMILY=="Lethrinidae",list(EST_LBS=sum(EST_LBS)),by=list(SPECIES_FK,SCIENTIFIC_NAME)]
+Prop.Rub    <- Prop.Emp[SPECIES_FK=="267"]$EST_LBS/sum(Prop.Emp[SPECIES_FK!="260"]$EST_LBS)
+Prop.Rub    <- round(Prop.Rub,3)
 
-# For all interview records (using CATCH_PK variable) of P. flavipinnis or filamentosus for years between 2010 and 2015, randomly assign record as "P. flavi" proportionally to Prop.Flavi (all fish in an interview)
-CATCH_PK.list      <- unique(bbs_3C[YEAR>=2010&YEAR<=2015]$CATCH_PK)
+# For all interview records (using CATCH_PK variable) containing "emperors - 260" ID in Manua (all years), randomly assign record as to L. rubrio according to its proportion in 1986-2010
+CATCH_PK.list      <- unique(bbs_3C[AREA_B=="Manua"]$CATCH_PK)
 for (i in 1:length(CATCH_PK.list)){
   
   aCatch   <- bbs_3C[CATCH_PK==CATCH_PK.list[i]]
   aSpecies <- aCatch[1,SPECIES_FK] # Just check first line of the CATCH_PK (CATCH_PK is at the species level, so all lines should be the same species)
   
-  if(aSpecies=="241"|aSpecies=="242"){
+  if(aSpecies=="260"){
     
-    if(runif(n=1,0,1)<=Prop.Flavi){    
-      bbs_3C[CATCH_PK==CATCH_PK.list[i]]$SPECIES_FK2 <- "241"
+    if(runif(n=1,0,1)<=Prop.Rub){    
+      bbs_3C[CATCH_PK==CATCH_PK.list[i]]$SPECIES_FK2 <- "267"
     } else {
-      bbs_3C[CATCH_PK==CATCH_PK.list[i]]$SPECIES_FK2 <- "242"  
+      bbs_3C[CATCH_PK==CATCH_PK.list[i]]$SPECIES_FK2 <- "260"  
     }
   }
 }	
 
 
+# Test <- bbs_3C[,list(EST_LBS=max(EST_LBS)),by=list(YEAR,AREA_B,INTERVIEW_PK,CATCH_PK,FAMILY,SPECIES_FK2,SCIENTIFIC_NAME)]
+# Test <- Test[AREA_B=="Manua"&FAMILY=="Lethrinidae",list(EST_LBS=sum(EST_LBS)),by=list(SPECIES_FK2)]
+# Prop.Rub; Test[SPECIES_FK2=="267"]$EST_LBS/sum(Test$EST_LBS)
 
 
+# save in the output folder.
+saveRDS(bbs_3C,file=paste(paste0(root_dir, "/Outputs/CPUE_processed.rds")))
 
-
-
-
-
-
-
-
-
-		# Investigate: list total catch, by species, for Lethrinidae in Manu'a Islands
-
-		string <- "SELECT SPECIES_FK, SCIENTIFIC_NAME, sum(EST_LBS) AS tot_catch
-		  FROM (
-			SELECT DISTINCT SPECIES_FK, SCIENTIFIC_NAME, EST_LBS
-			FROM bbs_3C
-		  	WHERE AREA_B = 'Manua' AND SPECIES_FK in ('254','255','260','261','262','263','264','266',
-				'267','2601','5065','5075','5106','5175','5191','5207','5221','5262'))
-		GROUP BY SCIENTIFIC_NAME
-		  "
-		manua_emperors <- sqldf(string, stringsAsFactors=FALSE)		
-			#only 3 species were identified:
-			#  262      Lethrinus amboinensis   183.914
-			#  261        Lethrinus elongatus    26.314
-			#  267 Lethrinus rubrioperculatus   125.315
-
-		# Prepare a check: total lbs surveyed in bbs_3C
-		string <- "SELECT DISTINCT CATCH_PK, EST_LBS
-		  FROM bbs_3C
-		  "
-		tot_catch_check_0 <- sqldf(string, stringsAsFactors=FALSE)
-		sum(tot_catch_check_0$EST_LBS, na.rm=TRUE)					# 442824.1
-
-		# prepare a check: all lethrinidae, manu'a, 1986-2021
-		string <- "SELECT DISTINCT CATCH_PK, EST_LBS
-		  FROM bbs_3C
-		  WHERE SPECIES_FK in ('262','261','267','260') AND AREA_B = 'Manua'
-		  "
-		manuaemps_check <- sqldf(string, stringsAsFactors=FALSE)	
-		sum_manuaemps <- sum(manuaemps_check$EST_LBS)				# 2078.13 lbs
-
-	# calculate sum(each id species) / sum(all id species)
-  	string <- "SELECT SPECIES_FK, SCIENTIFIC_NAME, SUM(EST_LBS) as TOT_LBS
-		  FROM
-			(SELECT DISTINCT CATCH_PK, SPECIES_FK, SCIENTIFIC_NAME, EST_LBS
-			FROM bbs_3C
-			WHERE SPECIES_FK in ('262','261','267') AND AREA_B = 'Manua') 
-		  GROUP BY SPECIES_FK"
-	manuaemps_1 <- sqldf(string, stringsAsFactors=FALSE)
-	p_elongatus <- manuaemps_1$TOT_LBS[1]/(sum(manuaemps_1$TOT_LBS))
-	p_amboinensis <- manuaemps_1$TOT_LBS[2]/(sum(manuaemps_1$TOT_LBS))
-	p_rubrio <- manuaemps_1$TOT_LBS[3]/(sum(manuaemps_1$TOT_LBS))
-
-	# list all interview_pk, catch_pk that included id and unid emperors, 1986-2020
-	string <- "SELECT DISTINCT YEAR, INTERVIEW_PK, CATCH_PK, SPECIES_FK, SCIENTIFIC_NAME, EST_LBS
-		  	FROM bbs_3C
-			WHERE SPECIES_FK in ('262','261','267','260') AND AREA_B = 'Manua'
-		  	"
-	manuaemps_2 <- sqldf(string, stringsAsFactors=FALSE)	#str(manuaemps_2)		#View(manuaemps_2)  #sum(manuaemps_2$EST_LBS)
-	# 148 catch records, 2078.13 lbs total
+#save.image(paste(root_dir, "/Outputs/01_BBS_data_prep.RData", sep=""))
 	
-	# divide interviews into those which caught just 1 lethrinidae, and those which caught 2+
-		# add N_CATCH_PK column
-		string <- "SELECT INTERVIEW_PK, count(CATCH_PK) as N_CATCH_PK
-			FROM (SELECT DISTINCT INTERVIEW_PK, CATCH_PK
-		  		FROM manuaemps_2)
-			GROUP BY INTERVIEW_PK
-			ORDER BY N_CATCH_PK
-			"
-		both_sp_question <- sqldf(string, stringsAsFactors=FALSE)			#View(both_sp_question)	
-
-		string <- "SELECT manuaemps_2.*, both_sp_question.N_CATCH_PK
-					FROM manuaemps_2 LEFT JOIN both_sp_question
-						ON manuaemps_2.INTERVIEW_PK = both_sp_question.INTERVIEW_PK"
-		manuaemps_2B <- sqldf(string, stringsAsFactors=FALSE)		# sum(manuaemps_2B$EST_LBS)		#View(manuaemps_2B)
-
-		just_1_sp <- subset(manuaemps_2B, N_CATCH_PK == 1)			# View(just_1_sp)
-		just_1_spB <- just_1_sp[,c(2,3,6)]						# View(just_1_spB)
-			rownames(just_1_spB) <- NULL
-		both_sp <- subset(manuaemps_2B, N_CATCH_PK > 1)				# View(both_sp)		#sum(both_sp$EST_LBS)
-
-		#  ----- CAREFUL!!!
-		#   Always make sure this is happening the way we expect.
-		string <- "SELECT INTERVIEW_PK, CATCH_PK, sum(EST_LBS) as sum_manuaemps
-			FROM both_sp
-			GROUP BY INTERVIEW_PK
-			"
-		both_sp2 <- sqldf(string, stringsAsFactors=FALSE)	#View(both_sp2)		#sum(both_sp2$sum_manuaemps)	
-		names(both_sp2)[3] <- 'EST_LBS'				#sum(both_sp2$EST_LBS)	#74.856, we expect this to be small.
-
-		# List the CATCH_PK (eliminated in previous step) that we will drop later:
-		drop_catch_pk <- subset(both_sp, !(CATCH_PK %in% both_sp2$CATCH_PK))
-
-	manuaemps_2C <- rbind(just_1_spB, both_sp2)						# sum(variola_2C$EST_LBS)	
-
-	# make a column of EST_LBS for each species
-	manuaemps_3 <- mutate(manuaemps_2C, ELONGATUS_LBS = round(EST_LBS*p_elongatus,3), 
-				AMBOINENSIS_LBS = round(EST_LBS*p_amboinensis,3),
-				RUBRIO_LBS = round(EST_LBS*p_rubrio,3))	  
-			# str(manuaemps_3)   #2078.121, not 2078.13, that is simply rounding loss.
-			# sum(manuaemps_3$ELONGATUS_LBS)+sum(manuaemps_3$AMBOINENSIS_LBS)+sum(manuaemps_3$RUBRIO_LBS)
-
-	# loop through manuaemps_3. For each original CATCH_PK, we must create a new CATCH_PK&A, CATCH_PK&E, and CATCH_PK&R 
-	#	for each Lethrinus species identified in the data
-	  # create dummy df to append to
-	  manuaemps_4 <- data.frame(INTERVIEW_PK = 9999, CATCH_PK = 'abc', 
-				NEW_CATCH_PK = 'abc', SPECIES_FK = 'abc', SCIENTIFIC_NAME = 'abc', EST_LBS = 9999)		#str(manuaemps_4)		
-	
-	  for (i in 1:nrow(manuaemps_3)) {
-		 add_ambo <- data.frame(INTERVIEW_PK = manuaemps_3$INTERVIEW_PK[i],
-					CATCH_PK = manuaemps_3$CATCH_PK[i], NEW_CATCH_PK = paste(manuaemps_3$CATCH_PK[i],'A',sep=""), 
-					SPECIES_FK = '262', SCIENTIFIC_NAME = 'Lethrinus amboinensis', EST_LBS = manuaemps_3$AMBOINENSIS_LBS[i])
-		 add_elong <- data.frame(INTERVIEW_PK = manuaemps_3$INTERVIEW_PK[i],
-					CATCH_PK = manuaemps_3$CATCH_PK[i], NEW_CATCH_PK = paste(manuaemps_3$CATCH_PK[i],'E',sep=""), 
-					SPECIES_FK = '261', SCIENTIFIC_NAME = 'Lethrinus elongatus', EST_LBS = manuaemps_3$ELONGATUS_LBS[i])
- 		add_rubrio <- data.frame(INTERVIEW_PK = manuaemps_3$INTERVIEW_PK[i],
-					CATCH_PK = manuaemps_3$CATCH_PK[i], NEW_CATCH_PK = paste(manuaemps_3$CATCH_PK[i],'R',sep=""), 
-					SPECIES_FK = '267', SCIENTIFIC_NAME = 'Lethrinus rubrioperculatus', EST_LBS = manuaemps_3$RUBRIO_LBS[i])
-		manuaemps_4 <- rbind(manuaemps_4,add_ambo, add_elong, add_rubrio)
-	    }								#  head(manuaemps_4)
- 
-	  # drop dummy row 1
-	  manuaemps_4 <- manuaemps_4[-1,]
-
-	  # CHECK
-	  #sum(manuaemps_4$EST_LBS)		#2078.121
-	  update_interview_pks <- unique(manuaemps_4$INTERVIEW_PK)
-
-	# update bbs_3C with these new records
-		rownames(manuaemps_4) <- NULL
-	
-	bbs_3C_keep1 <- subset(bbs_3C, !(CATCH_PK %in% manuaemps_4$CATCH_PK))	
-		#str(bbs_3C_keep1)	#48740 records
-
-	# don't forget, we had to drop a CATCH_PK for rare interviews with multiple lethrinus / lethrinidae were recorded
-	#	because we summed that catch under just 1 CATCH_PK per interview. eliminate those catch_pk that we dropped now
-	#	to avoid double counting catch
-	bbs_3C_keep2 <- subset(bbs_3C_keep1, !(CATCH_PK %in% drop_catch_pk$CATCH_PK))		#str(bbs_3C_keep2)  #48733 
-		
-	bbs_3C_update1 <- subset(bbs_3C, (CATCH_PK %in% manuaemps_4$CATCH_PK))	#str(bbs_3C_update1)	#171 records
-	# View(head(bbs_3C_update1))	
-
-	# keep only the first instance of each CATCH_PK (because some catch_pk had lengths)
-	bbs_3C_update2 <- bbs_3C_update1[with(bbs_3C_update1, order(CATCH_PK)), ]
-		
-	# keep only the first CATCH_PK for each INTERVIEW_PK
-	bbs_3C_update3 <- bbs_3C_update2[match(unique(bbs_3C_update2$CATCH_PK), bbs_3C_update2$CATCH_PK),]	
-		#str(bbs_3C_update3)	#143 records
-
-	# merge on the updated CATCH_PKs
-	string <- "SELECT bbs_3C_update3.*, manuaemps_4.NEW_CATCH_PK, manuaemps_4.SPECIES_FK as NEW_SPECIES_FK,
-				manuaemps_4.SCIENTIFIC_NAME as NEW_SCIENTIFIC_NAME, manuaemps_4.EST_LBS as NEW_EST_LBS
-					FROM bbs_3C_update3 LEFT JOIN manuaemps_4
-						ON bbs_3C_update3.CATCH_PK = manuaemps_4.CATCH_PK"
-		bbs_3C_update4 <- sqldf(string, stringsAsFactors=FALSE)		
-			#View(bbs_3C_update4)		#sum(bbs_3C_update4$NEW_EST_LBS)			#2078.121		#names(bbs_3C_update4)
-
-	# use NEW_ columns in place of the original (retain same column order as bbs_3C)		#names(bbs_3C_update4)
-	bbs_3C_update4$CATCH_PK <- bbs_3C_update4$NEW_CATCH_PK
-	bbs_3C_update4$SPECIES_FK  <- bbs_3C_update4$NEW_SPECIES_FK
-	bbs_3C_update4$SCIENTIFIC_NAME  <- bbs_3C_update4$NEW_SCIENTIFIC_NAME
-	bbs_3C_update4$EST_LBS  <- bbs_3C_update4$NEW_EST_LBS
-	# drop columns 98 to 101
-	bbs_3C_update5 <- bbs_3C_update4[,1:97]   #sum(bbs_3C_update5$EST_LBS)	#2078.121
-
-	# put back together with the keep records
-	bbs_3C_new <- rbind(bbs_3C_keep2, bbs_3C_update5)
-
-	# repeat the check: all manua lethrinus / lethrinidae, 1986-2020
-		string <- "SELECT DISTINCT CATCH_PK, EST_LBS
-		  FROM bbs_3C_new
-		  WHERE SPECIES_FK in ('262','261','267','260') AND AREA_B = 'Manua'
-		  "
-		manuaemps_check_again <- sqldf(string, stringsAsFactors=FALSE)	
-		sum(manuaemps_check_again$EST_LBS)				# 2078.1321 lbs
-
-	# also check that bbs_3C still contains 3068 interviews and total lbs surveyed is the same
-	length(unique(bbs_3C$INTERVIEW_PK))
-	#nrow(bbs_3C_new)  	#we expect lost rows because we dropped lengths and some CATCH_PK
-		string <- "SELECT DISTINCT CATCH_PK, EST_LBS
-		  FROM bbs_3C_new
-		  "
-		tot_catch_check <- sqldf(string, stringsAsFactors=FALSE)
-		
-
-	sum(tot_catch_check$EST_LBS, na.rm=TRUE)					# 442824.1
-
-	# update bbs_3C			
-	bbs_3C <- bbs_3C_new
-
-  # clean up workspace
-  	all_objs <- ls()
-  	save_objs <- c("aint_bbs_all_gears","aint_bbs_filtered","bbs_3C","root_dir","bbs_3C_before_ID_correct",
- 					"p_louti", "p_albimarginata","p_flavi","p_fila","p_elongatus",
-  					"p_amboinensis","p_rubrio")
-  	remove_objs <- setdiff(all_objs, save_objs)
   
-  if (1==2) {
-      rm(list=remove_objs)
-  	rm(save_objs)
-  	rm(remove_objs)
-  	rm(all_objs)
-	}
 
-  # save in the output folder.
-  # 	save.image(paste(root_dir, "/output/01_BBS_data_prep.RData", sep=""))
-  # save.image(paste(root_dir, "/Outputs/01_BBS_data_prep.RData", sep=""))
-	
-  #	load(paste(root_dir, "/output/01_BBS_data_prep.RData", sep=""))
-
-
-  # take a quick look at 2021 interviews
-    if (1==2) {
-		string <- " SELECT DISTINCT SAMPLE_DATE, INTERVIEW_PK, VESSEL_REGIST_NO, FISHING_METHOD 
-				FROM bbs_3C
-		  		WHERE year = 2021"
-		check2021 <- sqldf(string, stringsAsFactors=FALSE)	
-		View(check2021)
-	}
 
 
 
