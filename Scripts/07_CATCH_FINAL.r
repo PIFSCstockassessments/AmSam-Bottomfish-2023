3e783609c2d57b41c7c6d4a72d9d09d0564c402e
Marc Nadon <marc.nadon@noaa.gov>
2022-05-02 15:56:26 -1000

    Update 07_CATCH_FINAL.r

--- Scripts/07_CATCH_FINAL.r
+++ Scripts/07_CATCH_FINAL.r
@@ -1,4 +1,4 @@
-require(dplyr); require(this.path); require(data.table);  require(openxlsx)
+require(dplyr); require(this.path); require(data.table);  require(openxlsx); require(ggplot2)
 
 
 root_dir <- this.path::here(..=1)
@@ -7,6 +7,7 @@ A <- readRDS(paste0(root_dir,"\\Outputs\\CATCH_BBS_A.rds")) # Boat-based catch
 B <- readRDS(paste0(root_dir,"\\Outputs\\CATCH_SBS_A.rds")) # Shore-based catch
 
 S <- read.xlsx(paste0(root_dir,"/Data/METADATA.xlsx"),sheet="BMUS")
+S$SPECIES_PK <- paste0("S",S$SPECIES_PK)
 S <- select(S,SPECIES_FK=SPECIES_PK,SPECIES)
 
 # Load historic data from Excel document
@@ -23,11 +24,14 @@ for(i in 1:length(Species.list)){
 D$SOURCE     <- "Historic"
 D$SD.LBS     <- 0
 D            <- merge(D,S,by="SPECIES")
-D$SPECIES_FK <- paste0("S",D$SPECIES_FK)
 D            <- select(D,SOURCE,SPECIES_FK,YEAR=Year,AREA_C=Area,LBS=lbs,SD.LBS)
 
 # Put all catches together
 E <- rbind(A,B,D)
+E <- merge(E,S,by="SPECIES_FK")
 
-ggplot(data=E,aes(x=YEAR,y=LBS,fill=AREA_C))+geom_bar(stat="identity",position="stack")+facet_wrap(~SPECIES_FK,scales="free_y")
+ggplot(data=E,aes(x=YEAR,y=LBS,fill=AREA_C))+geom_bar(stat="identity",position="stack")+facet_wrap(~SPECIES,scales="free_y")
 
+F <- E[YEAR>=1986,list(LBS=sum(LBS)),by=list(SPECIES,YEAR)]
+F <- F[order(SPECIES,YEAR)]
+write.xlsx(F,"MarcCatch.xlsx")
