1c2f85ceffcf6e164f5c6d5ee7356952ffe025d6
Marc Nadon <marc.nadon@noaa.gov>
2022-05-03 09:30:24 -1000

    Renamed some files

--- Scripts/00_Run all data processing scripts.r
+++ Scripts/00_Run all data processing scripts.r
@@ -17,7 +17,8 @@ source(paste0(this.path::here(.. = 1),"/Scripts/03_CPUE_BBS_PropTable.r"));  rm(
 source(paste0(this.path::here(.. = 1),"/Scripts/04_CATCH_BBS_Prep.r"));      rm(list=ls()) 
 source(paste0(this.path::here(.. = 1),"/Scripts/05_CATCH_SBS_PropTable.r")); rm(list=ls()) 
 source(paste0(this.path::here(.. = 1),"/Scripts/06_CATCH_SBS_Prep.r"));      rm(list=ls()) 
-source(paste0(this.path::here(.. = 1),"/Scripts/07_SIZE.r"));                rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/07_CATCH_FINAL.r"));                rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/08_SIZE.r"));                rm(list=ls()) 
 
 
 
