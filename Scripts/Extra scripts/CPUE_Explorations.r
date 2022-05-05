de932df6d77fc526bb43dc6dab4a50b755b2740a
Marc Nadon <marc.nadon@noaa.gov>
2022-05-05 12:00:42 -1000

    Update CPUE_Explorations.r

--- Scripts/Extra scripts/CPUE_Explorations.r
+++ Scripts/Extra scripts/CPUE_Explorations.r
@@ -5,6 +5,33 @@ root_dir <- this.path::here(.. = 2) # establish directories using this.path
 
 B <- readRDS(paste(paste0(root_dir, "/Outputs/CPUE_A.rds")))
 
+# How many interviews?
+I <- B[,list(ONES=1),by=list(YEAR,INTERVIEW_PK,METHOD_FK,BMUS)] # 3048 interviews
+I <- dcast(I,INTERVIEW_PK+METHOD_FK~BMUS,value.var="ONES")
+
+
+
+
+
+
+I[IS.BMUS==1|IS.BMUS.GROUP==1]
+
+
+table(B$METHOD_FK)
+
+# Explore the number of vessels with a registration number
+Test <- B[,list(N=.N),by=list(INTERVIEW_PK,VESSEL_REGIST_NO)]
+Test <- data.table(  table(Test$VESSEL_REGIST_NO)  )
+Test[V1=="NULL"]$N/sum(Test$N) # 15% of interviews contain vessel_regist_no == NULL
+
+
+
+
+# Explore patterns related to how many METHODS are contained with a single INTERVIEW_PK
+Test <- B[,list(ONES=1),by=list(INTERVIEW_PK,METHOD_FK)]
+Test <- Test[,list(N=sum(ONES)),by=list(INTERVIEW_PK)]   
+Test[N>1] # This shows an INTERVIEW_PK always contains a single METHOD   
+
 
 # Explore patterns related to interviews with BOTH species-level BMUS and potential group-level BMUS 
 B$BMUS <- 0
@@ -24,4 +51,9 @@ Test[BMUS>0&GROUPID>0]$BOTH <- 1
 
 Test <- Test[,list(NTOT=.N,BOTH=sum(BOTH)),by=list(YEAR)]
 
-ggplot(data=Test,aes(x=YEAR))+geom_line(aes(y=BOTH),col="red")+geom_line(aes(y=NTOT),col="black")
+Test$PERC <- Test$BOTH/Test$NTOT*100
+ggplot(data=Test,aes(x=YEAR,y=PERC))+geom_point(col="red")+geom_smooth() # This shows many interviews pre-2016 where both BMUS and BMUS.GROUPS are present.
+
+
+
+
