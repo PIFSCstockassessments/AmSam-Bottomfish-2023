ae887d106ca7594ca0442bda5336a568519af067
Marc Nadon <marc.nadon@noaa.gov>
2022-05-06 10:15:58 -1000

    Update 02c_CPUE_BBS_Final.r

--- Scripts/02c_CPUE_BBS_Final.r
+++ Scripts/02c_CPUE_BBS_Final.r
@@ -12,16 +12,25 @@ C <- C[METHOD_FK==4]; length(unique(C$INTERVIEW_PK))
 W <- readRDS(paste0(root_dir,"/Outputs/CPUE_WIND.rds"))
 C <- merge(C,W,by="INTERVIEW_PK",all.x=T)
 
+length(unique(C$INTERVIEW_PK))
 length(unique(C[is.na(WINDSPEED)]$INTERVIEW_PK)) # 385 interviews are missing Windspeed
 table((C[is.na(WINDSPEED)]$AREA_C))
 table((C[is.na(WINDSPEED)]$YEAR))
 
+# Add PCs targeting information
+PC <- readRDS(paste0(root_dir,"/Outputs/CPUE_PCA.rds"))
+C  <- merge(C,PC,by="INTERVIEW_PK",all.x=T)
+
+length(unique(C$INTERVIEW_PK))
+length(unique(C[is.na(PC1)]$INTERVIEW_PK)) # No interviews are missing PCs
+
 # Select final covariates for CPUE standardization
 length(unique(C$INTERVIEW_PK))
+
 C <- select(C,INTERVIEW_PK,YEAR,MONTH,TYPE_OF_DAY,AREA_C,WINDSPEED,PROP_UNID,BMUS,SPECIES_FK,HOURS_FISHED,NUM_GEAR,EST_LBS)
+C <- C[order(YEAR,MONTH,TYPE_OF_DAY,AREA_C,SPECIES_FK)]
 
-
-
+saveRDS(C,paste0(root_dir,"/Outputs/CPUE_B.rds"))
 
 
 
