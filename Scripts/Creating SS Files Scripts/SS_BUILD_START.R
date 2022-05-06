2f7193afa69f9051107f50ee5fe94b80dcfe93b6
MOshima-PIFSC <megumi.oshima@noaa.gov>
2022-05-06 12:22:25 -0400

    fixes to build_ctl, build_starter, and build_fore

--- Scripts/Creating SS Files Scripts/SS_BUILD_START.R
+++ Scripts/Creating SS Files Scripts/SS_BUILD_START.R
@@ -11,27 +11,34 @@ require(tidyverse)
 require(this.path)
 
 root_dir <- this.path::here(.. = 2)
-## specify species that you are creating files for. 
-## If you want to do all at once can make into a loop (for i in 1:nrow(Species.List))
-species  <- Species.List[1,1]
+seed <- 0123
+last_est_phs <- 10
+init_values <- 0
+N_boot <- 1
 
 ## Starter file
 #  --------------------------------------------------------------------------------------------------------------
-## STEP 1. Read in template starter file
+for(i in seq_along(Species.List$SPECIES)){
+  
+  species  <- Species.List$SPECIES[i]
+  
+  ## STEP 1. Read in template starter file
+  START <- r4ss::SS_readstarter(file = file.path(root_dir, "SS3 models", "TEMPLATE_FILES", "starter.ss"))
+  
+  ## STEP 2. Make any changes necessary
+  #  --------------------------------------------------------------------------------------------------------------
+  ## most likely changes are listed below: 
+  START$datfile <- "data.ss"
+  START$ctlfile <- "control.ss"
+  START$init_values_src <- init_values #switch 1 if want to use parameter values from par.ss
+  #START$parmtrace <- 0 #can switch to 1 to turn on, helpful for debugging model
+  START$N_bootstraps <- N_boot #change to 3 if you want to run bootstrap
+  START$last_estimation_phase <- last_est_phs #turn to 0 if you don't want estimation
+  START$seed <- seed
+  
+  ## STEP 3. Save updated starter file
+  #  --------------------------------------------------------------------------------------------------------------
+  r4ss::SS_writestarter(START, dir = file.path(root_dir, "SS3 models", species), overwrite = TRUE)
+  
+}
 
-START <- r4ss::SS_readstarter(file = file.path(root_dir, "SS3 models", "TEMPLATE_FILES", "starter.ss"))
-
-## STEP 2. Make any changes necessary
-#  --------------------------------------------------------------------------------------------------------------
-## most likely changes are listed below: 
-START$datfile <- "data.ss"
-START$ctlfile <- "control.ss"
-START$init_values_src <- 0 #switch 1 if want to use parameter values from par.ss
-START$parmtrace <- 0 #can switch to 1 to turn on, helpful for debugging model
-START$N_bootstraps <- 1 #change to 3 if you want to run bootstrap
-START$last_estimation_phase <- 10 #turn to 0 if you don't want estimation
-START$seed <- 0123
-
-## STEP 3. Save updated starter file
-#  --------------------------------------------------------------------------------------------------------------
-r4ss::SS_writestarter(START, dir = file.path(root_dir, "SS3 models", species), overwrite = TRUE)
