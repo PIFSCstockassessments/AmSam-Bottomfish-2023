fd5706bde6e18c390d8a1f41b15bf34fb8c24631
Marc Nadon <marc.nadon@noaa.gov>
2022-05-12 09:17:03 -1000

    Update 02c_CPUE_BBS_Final.r

--- Scripts/02c_CPUE_BBS_Final.r
+++ Scripts/02c_CPUE_BBS_Final.r
@@ -3,13 +3,13 @@ options(scipen=999)		              # this option just forces R to never use scie
 root_dir <- this.path::here(.. = 1) # establish directories using this.path
 
 # Base processed data
-C <- readRDS(paste0(root_dir, "/Outputs/CPUE_A.rds")); length(unique(C$INTERVIEW_PK))
+C <- readRDS(paste0(root_dir, "/Outputs/CPUE_B.rds")); length(unique(C$INTERVIEW_PK))
 
 # Only select Bottomfishing (Method_FK==4) trips. Given that BTM includes hours spent trolling.
 C <- C[METHOD_FK==4]; length(unique(C$INTERVIEW_PK))
 
 # Remove years 1986-1987, most data collected in groups.
-C <- C[YEAR>=1988]
+C <- C[YEAR>=1988]; length(unique(C$INTERVIEW_PK))
 
 # Make sure HOURS_FISHED is available and exclude some extreme NUM_GEAR values
 C <- C[HOURS_FISHED>0&(NUM_GEAR>0&NUM_GEAR<20)]; length(unique(C$INTERVIEW_PK))
@@ -19,7 +19,7 @@ W <- readRDS(paste0(root_dir,"/Outputs/CPUE_WIND.rds"))
 C <- merge(C,W,by="INTERVIEW_PK",all.x=T)
 
 length(unique(C$INTERVIEW_PK))
-length(unique(C[is.na(WINDSPEED)]$INTERVIEW_PK)) # 386 interviews are missing Windspeed
+length(unique(C[is.na(WINDSPEED)]$INTERVIEW_PK)) # 86 interviews are missing Windspeed
 table((C[is.na(WINDSPEED)]$AREA_C))
 table((C[is.na(WINDSPEED)]$YEAR))
 
@@ -30,9 +30,6 @@ C  <- merge(C,PC,by="INTERVIEW_PK",all.x=T)
 length(unique(C$INTERVIEW_PK))
 length(unique(C[is.na(PC1)]$INTERVIEW_PK)) # No interviews are missing PCs
 
-# Select final covariates for CPUE standardization
-length(unique(C$INTERVIEW_PK))
-
 # Fill all interviews with zero catches for all species
 S <- C[,list(N=.N),by=list(BMUS,SPECIES_FK)]
 S <- select(S,-N)
@@ -40,7 +37,7 @@ S <- select(S,-N)
 C <- C[,list(EST_LBS=sum(EST_LBS)),by=list(INTERVIEW_PK,YEAR,SEASON,MONTH,TYPE_OF_DAY,AREA_C,WINDSPEED,PROP_UNID,PC1,PC2,PC3,PC4,SPECIES_FK,HOURS_FISHED,NUM_GEAR)]
 C <- dcast(C,INTERVIEW_PK+YEAR+SEASON+MONTH+TYPE_OF_DAY+AREA_C+WINDSPEED+PROP_UNID+PC1+PC2+PC3+PC4+HOURS_FISHED+NUM_GEAR~SPECIES_FK,value.var="EST_LBS",fill=0)
 C <- melt(C,id.vars=1:14,value.name="EST_LBS",variable.name="SPECIES_FK")
-C <- data.table(C)
+C <- data.table(C); length(unique(C$INTERVIEW_PK))
 
 # Only keep the bmus species (we don't need the rest after the PCA analysis is done)
 C <- merge(C,S,by="SPECIES_FK")
@@ -65,5 +62,5 @@ C$PRES         <- ifelse(C$CPUE>0,1,0)
 C$SPECIES_FK   <- as.character(C$SPECIES_FK)
 
 length(unique(C$INTERVIEW_PK))
-saveRDS(C,paste0(root_dir,"/Outputs/CPUE_B.rds"))
+saveRDS(C,paste0(root_dir,"/Outputs/CPUE_C.rds"))
 
