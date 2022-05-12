c81f51ba199deddc6a76873dc7db7ca45b648263
Marc Nadon <marc.nadon@noaa.gov>
2022-05-11 14:36:56 -1000

    Update CPUE_Explorations.r

--- Scripts/Extra scripts/CPUE_Explorations.r
+++ Scripts/Extra scripts/CPUE_Explorations.r
@@ -7,7 +7,7 @@ B <- readRDS(paste(paste0(root_dir, "/Outputs/CPUE_A.rds")))
 
 # How many interviews?
 I <- B[,list(ONES=1),by=list(YEAR,INTERVIEW_PK,METHOD_FK,BMUS)] 
-I <- dcast(I,INTERVIEW_PK+YEAR+METHOD_FK~BMUS,value.var="ONES",fill=0) # 3048 interviews (Method 4 or 5)
+I <- dcast(I,INTERVIEW_PK+YEAR+METHOD_FK~BMUS,value.var="ONES",fill=0) # 3026 interviews (Method 4 or 5)
 
 nrow(I[METHOD_FK==4])/(nrow(I[METHOD_FK==4])+nrow(I[METHOD_FK==5])) # 75% of interviews are Bottomfishing
 I <- I[METHOD_FK==4]
@@ -15,6 +15,8 @@ I <- I[METHOD_FK==4]
 I$BOTH            <- ifelse(I$BMUS_Containing_Group==1&I$BMUS_Species==1,1,0)
 I2                <- I[BMUS_Containing_Group==1|BMUS_Species==1,list(GROUP=sum(BMUS_Containing_Group),BSPECIES=sum(BMUS_Species),BOTH=sum(BOTH)),by=list(YEAR)]
 I2$N_ONLY_SPECIES <- I2$BSPECIES-I2$BOTH
+I                 <- I[,]
+
 ggplot(data=I2,aes(x=YEAR,y=N_ONLY_SPECIES))+geom_line()
        # If we filtered for only interviews where ALL the BMUS catch is identified at the species level, we would get only about 10 to 30 interviews per year pre-2016.
 
