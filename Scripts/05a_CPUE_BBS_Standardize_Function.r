b3e7379a5f75ccbe33c7fc9e6c44df2ae08b5297
Marc Nadon <marc.nadon@noaa.gov>
2022-05-15 09:48:18 -1000

    Update 05a_CPUE_BBS_Standardize_Function.r

--- Scripts/05a_CPUE_BBS_Standardize_Function.r
+++ Scripts/05a_CPUE_BBS_Standardize_Function.r
@@ -30,36 +30,28 @@ YR.CATCH <- D[,list(CPUE=sum(CPUE)),by=YEAR]
 YR.CATCH <- YR.CATCH[CPUE>0]
 YR.CATCH <- select(YR.CATCH,-CPUE)
 D        <- merge(D,YR.CATCH,by="YEAR")
-D        <- droplevels(D)
 
-# Add data point weights so that each YxMxR strata have the same weight in the GAM binomial models
-if(Ar=="Tutuila"){ WGHT.B <- data.table(  table(D$YEAR,D$SEASON,D$AREA_C)  ); setnames(WGHT.B,c("YEAR","SEASON","AREA_C","N")) }
-if(Ar=="Manua")  { WGHT.B <- data.table(  table(D$YEAR,D$SEASON)  )         ; setnames(WGHT.B,c("YEAR","SEASON","N"))          }
-
-WGHT.B$Nobs_Nstrata   <- sum(WGHT.B$N)/nrow(WGHT.B[N>0]) # Add the Nobs / Nstrata ratio
-WGHT.B$W.B            <- WGHT.B$Nobs_Nstrata * 1/WGHT.B$N
-WGHT.B[W.B==Inf]$W.B  <- 0
-WGHT.B                <- WGHT.B[order(YEAR,SEASON)]
-WGHT.B                <- select(WGHT.B,-N,-Nobs_Nstrata)
-
-# Add data weights so that each YxMxR strata have the same weight in the GAM models
-if(Ar=="Tutuila"){ WGHT.P <- data.table(  table(D[CPUE>0]$YEAR,D[CPUE>0]$SEASON,D[CPUE>0]$AREA_C)  ); setnames(WGHT.P,c("YEAR","SEASON","AREA_C","N")) }
-if(Ar=="Manua")  { WGHT.P <- data.table(  table(D[CPUE>0]$YEAR,D[CPUE>0]$SEASON)  )                 ; setnames(WGHT.P,c("YEAR","SEASON","N"))          }
-
+# Add data point weights so that each YxMxR strata have the same weight in the GAM  models
+WGHT.P                <- data.table(  table(D[CPUE>0]$YEAR,D[CPUE>0]$SEASON,D[CPUE>0]$AREA_C)  ); setnames(WGHT.P,c("YEAR","SEASON","AREA_C","N"))# }
 WGHT.P$Nobs_Nstrata   <- sum(WGHT.P$N)/nrow(WGHT.P[N>0]) # Add the Nobs / Nstrata ratio (note: I use the number of strata with at least 1 interview)
 WGHT.P$W.P            <- WGHT.P$Nobs_Nstrata * 1/WGHT.P$N
 WGHT.P[W.P==Inf]$W.P  <- 0
 WGHT.P                <- WGHT.P[order(YEAR,SEASON)]
 WGHT.P                <- select(WGHT.P,-N,-Nobs_Nstrata)
+D                     <- merge(D,WGHT.P,by=c("YEAR","SEASON","AREA_C"),all.x=T) #}
 
-if(Ar=="Tutuila"){ D <- merge(D,WGHT.B,by=c("YEAR","SEASON","AREA_C"),all.x=T)
-                   D <- merge(D,WGHT.P,by=c("YEAR","SEASON","AREA_C"),all.x=T) }
-
-if(Ar=="Manua"){   D <- merge(D,WGHT.B,by=c("YEAR","SEASON"),all.x=T)
-                   D <- merge(D,WGHT.P,by=c("YEAR","SEASON"),all.x=T) }
+WGHT.B                <- data.table(  table(D$YEAR,D$SEASON,D$AREA_C)  ); setnames(WGHT.B,c("YEAR","SEASON","AREA_C","N")) # }
+WGHT.B$Nobs_Nstrata   <- sum(WGHT.B$N)/nrow(WGHT.B[N>0]) # Add the Nobs / Nstrata ratio
+WGHT.B$W.B            <- WGHT.B$Nobs_Nstrata * 1/WGHT.B$N
+WGHT.B[W.B==Inf]$W.B  <- 0
+WGHT.B                <- WGHT.B[order(YEAR,SEASON)]
+WGHT.B                <- select(WGHT.B,-N,-Nobs_Nstrata)
+D                     <- merge(D,WGHT.B,by=c("YEAR","SEASON","AREA_C"),all.x=T)
 
 # Backward selection: Positive catch-only models
-Model.String  <- 'gam(data=D[CPUE>0],weights=W.P,log(CPUE)~YEAR+s(HOURS_FISHED,k=3)+s(NUM_GEAR,k=3)+SEASON+s(PC1)+s(PC2)+s(PC3)+s(PC4)+TYPE_OF_DAY+AREA_C, method="REML")'
+if(Ar=="Tutuila") Model.String  <- 'gam(data=D[CPUE>0],weights=W.P,log(CPUE)~YEAR+s(HOURS_FISHED,k=3)+s(NUM_GEAR,k=3)+SEASON+s(PC1)+s(PC2)+s(PC3)+s(PC4)+TYPE_OF_DAY+AREA_C, method="REML")'
+if(Ar=="Manua")   Model.String  <- 'gam(data=D[CPUE>0],weights=W.P,log(CPUE)~YEAR+s(HOURS_FISHED,k=3)+s(NUM_GEAR,k=3)+SEASON+s(PC1)+s(PC2)+s(PC3)+s(PC4)+TYPE_OF_DAY, method="REML")'
+  
 aModel        <- eval(parse(text=Model.String))
 PreviousAIC   <- AIC(aModel)
 P.SelResults  <- data.table(DESCRIPTION="Full model",FORMULA=as.character(aModel$formula[3]),AIC=PreviousAIC,DELT_AIC=0)
@@ -79,7 +71,6 @@ for(i in 1:10){
   Diff          <- NewAIC-PreviousAIC
   
   if(Diff>2){ break; } else { 
-    
     P.SelResults <- rbind(P.SelResults,data.table(DESCRIPTION="Reduced model",FORMULA=gsub("+","-",RM,fixed=T),AIC=NewAIC,DELT_AIC=Diff))
     PreviousAIC  <- NewAIC 
     LastModel    <- aModel
@@ -88,11 +79,13 @@ for(i in 1:10){
 
 Best.P.Model.Formula <- gsub(" ","",as.character(LastModel$formula[3]))
 P.SelResults         <- rbind(P.SelResults,data.table(DESCRIPTION="Best model",FORMULA=as.character(LastModel$formula[3]),AIC=AIC(LastModel),DELT_AIC=0))
-P.SelResults$TYPE    <- "Positive-only" 
+P.SelResults$CPUE_TYPE    <- "Positive-only CPUE" 
 P.SelResults         <- select(P.SelResults,TYPE,DESCRIPTION,FORMULA,AIC,DELT_AIC)
 
 # Backward selection: Probability of catch-only models
-Model.String  <- 'gam(data=D,weights=W.B,PRES~YEAR+s(HOURS_FISHED,k=3)+s(NUM_GEAR,k=3)+SEASON+s(PC1)+s(PC2)+s(PC3)+s(PC4)+TYPE_OF_DAY+AREA_C,family=binomial(link="logit"),method="REML")'
+if(Ar=="Tutuila") Model.String  <- 'gam(data=D,weights=W.B,PRES~YEAR+s(HOURS_FISHED,k=3)+s(NUM_GEAR,k=3)+SEASON+s(PC1)+s(PC2)+s(PC3)+s(PC4)+TYPE_OF_DAY+AREA_C,family=binomial(link="logit"),method="REML")'
+if(Ar=="Manua")   Model.String  <- 'gam(data=D,weights=W.B,PRES~YEAR+s(HOURS_FISHED,k=3)+s(NUM_GEAR,k=3)+SEASON+s(PC1)+s(PC2)+s(PC3)+s(PC4)+TYPE_OF_DAY,family=binomial(link="logit"),method="REML")'
+
 aModel        <- eval(parse(text=Model.String))
 PreviousAIC   <- AIC(aModel)
 B.SelResults  <- data.table(DESCRIPTION="Full model",FORMULA=as.character(aModel$formula[3]),AIC=PreviousAIC,DELT_AIC=0)
@@ -112,7 +105,6 @@ for(i in 1:10){
   Diff          <- NewAIC-PreviousAIC
   
   if(Diff>2){ break; } else { 
-    
     B.SelResults <- rbind(B.SelResults,data.table(DESCRIPTION="Reduced model",FORMULA=gsub("+","-",RM,fixed=T),AIC=NewAIC,DELT_AIC=Diff))
     PreviousAIC  <- NewAIC 
     LastModel    <- aModel
@@ -121,14 +113,13 @@ for(i in 1:10){
 
 Best.B.Model.Formula <- gsub(" ","",as.character(LastModel$formula[3]))
 B.SelResults         <- rbind(B.SelResults,data.table(DESCRIPTION="Best model",FORMULA=as.character(LastModel$formula[3]),AIC=AIC(LastModel),DELT_AIC=0))
-B.SelResults$TYPE    <- "Presence" 
+B.SelResults$CPUE_TYPE    <- "Probability CPUE" 
 B.SelResults         <- select(B.SelResults,TYPE,DESCRIPTION,FORMULA,AIC,DELT_AIC)
 
 # For model comparison purposes, take the final models and re-run simpler models to evaluate the effect of each variables
 P.Models      <- list()
 Model.String  <- paste0('gam(data=D[CPUE>0],weights=W.P,log(CPUE)~',Best.P.Model.Formula,',method="REML")')
 for(i in 1:20){
-  
   P.Models[[i]] <- eval(parse(text=Model.String))
   RM            <- gsub(" ","",as.character(P.Models[[i]]$formula[3]))
   RM            <- strsplit(RM,"\\+") 
@@ -152,29 +143,26 @@ for(i in 1:20){
 }
 
 # Make a list of model names for the figure legend
-P.Model.Names <- data.table(MODEL=as.factor(as.character(P.Models[[1]]$formula[3])),ORDER=1)
-TM            <- data.table(  MODEL=as.factor(as.character(strsplit( gsub(" ","",P.Model.Names$MODEL),"\\+"  )[[1]])), ORDER=1 )
-TM$ORDER      <- seq((nrow(TM)+1),2)
-P.Model.Names <- rbind(P.Model.Names,TM[-1,])
-P.Model.Names$MODEL <- fct_reorder(P.Model.Names$MODEL,P.Model.Names$ORDER,min)
-P.Model.Names <- select(P.Model.Names,-ORDER)
+P.Model.Names  <- data.table(MODEL=as.factor(as.character(P.Models[[1]]$formula[3])),MODEL_ORDER=1)
+TM             <- data.table(  MODEL=as.factor(as.character(strsplit( gsub(" ","",P.Model.Names$MODEL),"\\+"  )[[1]])), MODEL_ORDER=1 )
+TM$MODEL_ORDER <- seq((nrow(TM)+1),2)
+TM$MODEL       <- paste0("- ",TM$MODEL)
+P.Model.Names  <- rbind(P.Model.Names,TM[-1,])
+P.Model.Names$MODEL <- fct_reorder(P.Model.Names$MODEL,P.Model.Names$MODEL_ORDER,min)
 
-B.Model.Names <- data.table(MODEL=as.factor(as.character(B.Models[[1]]$formula[3])),ORDER=1)
-TM            <- data.table(  MODEL=as.factor(as.character(strsplit( gsub(" ","",B.Model.Names$MODEL),"\\+"  )[[1]])), ORDER=1 )
-TM$ORDER      <- seq((nrow(TM)+1),2)
+B.Model.Names <- data.table(MODEL=as.factor(as.character(B.Models[[1]]$formula[3])),MODEL_ORDER=1)
+TM            <- data.table(  MODEL=as.factor(as.character(strsplit( gsub(" ","",B.Model.Names$MODEL),"\\+"  )[[1]])), MODEL_ORDER=1 )
+TM$MODEL_ORDER      <- seq((nrow(TM)+1),2)
+TM$MODEL      <- paste0("- ",TM$MODEL)
 B.Model.Names <- rbind(B.Model.Names,TM[-1,])
-B.Model.Names$MODEL <- fct_reorder(B.Model.Names$MODEL,B.Model.Names$ORDER,min)
-B.Model.Names <- select(B.Model.Names,-ORDER)
+B.Model.Names$MODEL <- fct_reorder(B.Model.Names$MODEL,B.Model.Names$MODEL_ORDER,min)
 
 #========================Generate standardized CPUE index for all models========================================
 # Create Walter's large table template
-
-#if(Ar=="Tutuila"){
 WLT <- data.table(  table(D$YEAR,D$SEASON,D$AREA_C)  ); setnames(WLT,c("YEAR","SEASON","AREA_C","N")) #}
-#if(Ar=="Manua")  { WLT <- data.table(  table(D$YEAR,D$SEASON)  )         ; setnames(WLT,c("YEAR","SEASON","N"))          }
-WLT       <- select(WLT,-N)
+WLT <- select(WLT,-N)
 
-# Add median for continuous variables
+# Add median for continuous variables and most commmon variable for categorical ones
 WLT$HOURS_FISHED <- median(D$HOURS_FISHED)
 WLT$NUM_GEAR     <- median(D$NUM_GEAR)
 WLT$TYPE_OF_DAY  <- "WD"
@@ -192,86 +180,86 @@ WLT[AREA_C=="Bank"]$WEIGHT    <- 0.09
 Results.P <- list()
 for(i in 1:length(P.Models)){
   
-  aP.Model     <- P.Models[[i]]
-  aWLT         <- WLT
-  aWLT$MODEL.P <- P.Model.Names[i]
-
+  aP.Model         <- P.Models[[i]]
+  aWLT             <- WLT
+  aWLT$MODEL       <- P.Model.Names[i]$MODEL
+  aWLT$MODEL_ORDER <- P.Model.Names[i]$MODEL_ORDER
+  
   # Predict expected positive catch for all level combinations and calculate standard errors
-  LOG.POS.CPUE     <- predict.gam(aP.Model,newdata=WLT, se.fit=T)$fit
-  SD.LOG.POS.CPUE  <- predict.gam(aP.Model,newdata=WLT, se.fit=T)$se
-  aWLT             <- cbind(aWLT,LOG.POS.CPUE)
-  aWLT             <- cbind(aWLT,SD.LOG.POS.CPUE)
-  aWLT$POS.CPUE    <- exp(aWLT$LOG.POS.CPUE+aWLT$SD.LOG.POS.CPUE[i]^2/2 )
-  aWLT$LOWER95.POS <- exp(aWLT$LOG.POS.CPUE-1.96*aWLT$SD.LOG.POS.CPUE)
-  aWLT$UPPER95.POS <- exp(aWLT$LOG.POS.CPUE+1.96*aWLT$SD.LOG.POS.CPUE)
-  aWLT$SD.POS.CPUE <- (aWLT$UPPER95.POS-aWLT$LOWER95.POS)/3.92
-  Results.P[[i]]   <- select(aWLT,MODEL.P,YEAR,SEASON,AREA_C,WEIGHT,POS.CPUE,SD.POS.CPUE)
+  LOG.CPUE       <- predict.gam(aP.Model,newdata=WLT, se.fit=T)$fit
+  SD.LOG.CPUE    <- predict.gam(aP.Model,newdata=WLT, se.fit=T)$se
+  aWLT           <- cbind(aWLT,LOG.CPUE)
+  aWLT           <- cbind(aWLT,SD.LOG.CPUE)
+  aWLT$CPUE      <- exp(aWLT$LOG.CPUE+aWLT$SD.LOG.CPUE[i]^2/2 )
+  aWLT$LOWER95   <- exp(aWLT$LOG.CPUE-1.96*aWLT$SD.LOG.CPUE)
+  aWLT$UPPER95   <- exp(aWLT$LOG.CPUE+1.96*aWLT$SD.LOG.CPUE)
+  aWLT$SD.CPUE   <- (aWLT$UPPER95-aWLT$LOWER95)/3.92
+  aWLT$CPUE_TYPE <- "POS"
+  Results.P[[i]] <- select(aWLT,CPUE_TYPE,MODEL,MODEL_ORDER,YEAR,SEASON,AREA_C,WEIGHT,CPUE,SD.CPUE)
 }
 
 # Calculate standardize index for all probability of catch models
 Results.B <- list()
 for(i in 1:length(B.Models)){
   
-  aB.Model     <- B.Models[[i]]
-  aWLT         <- WLT
-  aWLT$MODEL.B <- B.Model.Names[i]
+  aB.Model         <- B.Models[[i]]
+  aWLT             <- WLT
+  aWLT$MODEL       <- B.Model.Names[i]$MODEL
+  aWLT$MODEL_ORDER <- B.Model.Names[i]$MODEL_ORDER
   
   # Predict expected positive catch for all level combinations and calculate standard errors
-  LOGIT.PROB.CPUE     <- predict.gam(aB.Model,newdata=aWLT,se.fit=T)$fit
-  SD.LOGIT.PROB.CPUE  <- predict.gam(aB.Model,newdata=aWLT,se.fit=T)$se
-  aWLT                <- cbind(aWLT,LOGIT.PROB.CPUE)
-  aWLT                <- cbind(aWLT,SD.LOGIT.PROB.CPUE)
-  aWLT$PROB.CPUE      <- inv.logit(aWLT$LOGIT.PROB.CPUE)
-  aWLT$LOWER95.PROB   <- inv.logit(aWLT$LOGIT.PROB.CPUE-1.96*aWLT$SD.LOGIT.PROB.CPUE)
-  aWLT$UPPER95.PROB   <- inv.logit(aWLT$LOGIT.PROB.CPUE+1.96*aWLT$SD.LOGIT.PROB.CPUE)
-  aWLT$SD.PROB.CPUE   <- (aWLT$UPPER95.PROB-aWLT$LOWER95.PROB)/3.92
-  Results.B[[i]]      <- select(aWLT,MODEL.B,YEAR,SEASON,AREA_C,WEIGHT,PROB.CPUE,SD.PROB.CPUE)
+  LOGIT.CPUE     <- predict.gam(aB.Model,newdata=aWLT,se.fit=T)$fit
+  SD.LOGIT.CPUE  <- predict.gam(aB.Model,newdata=aWLT,se.fit=T)$se
+  aWLT           <- cbind(aWLT,LOGIT.CPUE)
+  aWLT           <- cbind(aWLT,SD.LOGIT.CPUE)
+  aWLT$CPUE      <- inv.logit(aWLT$LOGIT.CPUE)
+  aWLT$LOWER95   <- inv.logit(aWLT$LOGIT.CPUE-1.96*aWLT$SD.LOGIT.CPUE)
+  aWLT$UPPER95   <- inv.logit(aWLT$LOGIT.CPUE+1.96*aWLT$SD.LOGIT.CPUE)
+  aWLT$SD.CPUE   <- (aWLT$UPPER95-aWLT$LOWER95)/3.92
+  aWLT$CPUE_TYPE <- "PROB"
+  Results.B[[i]] <- select(aWLT,CPUE_TYPE,MODEL,MODEL_ORDER,YEAR,SEASON,AREA_C,WEIGHT,CPUE,SD.CPUE)
 }
 
-# Calculate the combined CPUE for the best model only
-Best.Mod             <- merge(Results.P[[1]],Results.B[[1]],by=c("YEAR","SEASON","AREA_C","WEIGHT"))
-Best.Mod$TOT.CPUE    <- Best.Mod$POS.CPUE*Best.Mod$PROB.CPUE
-Best.Mod$SD.TOT.CPUE <- sqrt(  Best.Mod$SD.POS.CPUE^2*Best.Mod$SD.PROB.CPUE^2+Best.Mod$SD.POS.CPUE^2*Best.Mod$PROB.CPUE^2+Best.Mod$SD.PROB.CPUE^2*Best.Mod$POS.CPUE^2    )
+# Combine all results in one table
+Results <- rbind(rbindlist(Results.P),rbindlist(Results.B))
 
-Best.Mod <- Best.Mod[,list(TOT.CPUE=sum(TOT.CPUE*WEIGHT),SD.TOT.CPUE=sqrt(sum(SD.TOT.CPUE^2*WEIGHT^2)),POS.CPUE=sum(POS.CPUE*WEIGHT),PROB.CPUE=sum(PROB.CPUE*WEIGHT)),by=list(MODEL.P,MODEL.B,YEAR,SEASON)] # Sum abundance in all region, by regional weight
-Best.Mod <- Best.Mod[,list(TOT.CPUE=mean(TOT.CPUE),SD.TOT.CPUE=mean(SD.TOT.CPUE),POS.CPUE=mean(POS.CPUE),PROB.CPUE=mean(PROB.CPUE)),by=list(MODEL.P,MODEL.B,YEAR)] # Average all 12 SEASONs per year
+# Calculate the combined CPUE for the best model only
+Best.Mod <- Results[MODEL_ORDER==1]
+Best.Mod <- dcast(Best.Mod,YEAR+SEASON+AREA_C+WEIGHT~CPUE_TYPE,value.var=c("MODEL","CPUE","SD.CPUE"))
+
+Best.Mod$CPUE_TOT    <- Best.Mod$CPUE_POS*Best.Mod$CPUE_PROB
+Best.Mod$SD.CPUE_TOT <- sqrt(  Best.Mod$SD.CPUE_POS^2*Best.Mod$SD.CPUE_PROB^2+Best.Mod$SD.CPUE_POS^2*Best.Mod$CPUE_PROB^2+Best.Mod$SD.CPUE_PROB^2*Best.Mod$CPUE_POS^2    )
+
+Best.Mod <- Best.Mod[,list(CPUE_TOT=sum(CPUE_TOT*WEIGHT),SD.CPUE_TOT=sqrt(sum(SD.CPUE_TOT^2*WEIGHT^2)),CPUE_POS=sum(CPUE_POS*WEIGHT),CPUE_PROB=sum(CPUE_PROB*WEIGHT)),by=list(MODEL_POS,MODEL_PROB,YEAR,SEASON)] # Sum abundance in all region, by regional weight
+Best.Mod <- Best.Mod[,list(CPUE_TOT=mean(CPUE_TOT),SD.CPUE_TOT=mean(SD.CPUE_TOT),CPUE_POS=mean(CPUE_POS),CPUE_PROB=mean(CPUE_PROB)),by=list(MODEL_POS,MODEL_PROB,YEAR)] # Average all 12 SEASONs per year
 
 Best.Mod$YEAR            <- as.numeric(Best.Mod$YEAR)
-Best.Mod$TOT.CPUE.STD    <- Best.Mod$TOT.CPUE/mean(Best.Mod$TOT.CPUE)*100
-Best.Mod$SD.TOT.CPUE.STD <- Best.Mod$SD.TOT.CPUE/mean(Best.Mod$SD.TOT.CPUE)*100
-Best.Mod$POS.CPUE.STD    <- Best.Mod$POS.CPUE/mean(Best.Mod$POS.CPUE)*100
-Best.Mod$PROB.CPUE.STD   <- Best.Mod$PROB.CPUE/mean(Best.Mod$PROB.CPUE)*100
+Best.Mod$CPUE_TOT.STD    <- Best.Mod$CPUE_TOT/mean(Best.Mod$CPUE_TOT)*100
+Best.Mod$SD.CPUE_TOT.STD <- Best.Mod$SD.CPUE_TOT/mean(Best.Mod$SD.CPUE_TOT)*100
+Best.Mod$CPUE_POS.STD    <- Best.Mod$CPUE_POS/mean(Best.Mod$CPUE_POS)*100
+Best.Mod$CPUE_PROB.STD   <- Best.Mod$CPUE_PROB/mean(Best.Mod$CPUE_PROB)*100
 
 # Add nominal CPUE information
-NOMI1              <- D[PRES>0,list(POS.CPUE=mean(CPUE/HOURS_FISHED/NUM_GEAR)),by=list(YEAR)]
-NOMI               <- D[,list(TOT.CPUE=mean(CPUE),PROB.CPUE=mean(PRES)),by=list(YEAR)]
+NOMI1              <- D[PRES>0,list(CPUE_POS=mean(CPUE)),by=list(YEAR)]
+NOMI               <- D[,list(CPUE_TOT=mean(CPUE),CPUE_PROB=mean(PRES)),by=list(YEAR)]
 NOMI               <- merge(NOMI,NOMI1,by="YEAR")
-NOMI$TOT.CPUE.STD  <- NOMI$TOT.CPUE/mean(NOMI$TOT.CPUE)*100
-NOMI$PROB.CPUE.STD <- NOMI$PROB.CPUE/mean(NOMI$PROB.CPUE)*100
-NOMI$POS.CPUE.STD  <- NOMI$POS.CPUE/mean(NOMI$POS.CPUE)*100
-NOMI$MODEL.B       <- NOMI$MODEL.P <- "NOMI"
+NOMI$CPUE_TOT.STD  <- NOMI$CPUE_TOT/mean(NOMI$CPUE_TOT)*100
+NOMI$CPUE_PROB.STD <- NOMI$CPUE_PROB/mean(NOMI$CPUE_PROB)*100
+NOMI$CPUE_POS.STD  <- NOMI$CPUE_POS/mean(NOMI$CPUE_POS)*100
+NOMI$MODEL_PROB    <- NOMI$MODEL_POS <- "NOMI"
 NOMI$YEAR          <- as.numeric(as.character((NOMI$YEAR)))
-NOMI               <- select(NOMI,MODEL.B,MODEL.P,YEAR,TOT.CPUE.STD,POS.CPUE.STD,PROB.CPUE.STD)
+NOMI               <- select(NOMI,MODEL_PROB,MODEL_POS,YEAR,CPUE_TOT.STD,CPUE_POS.STD,CPUE_PROB.STD)
 
-Best.Mod <- rbind(select(Best.Mod,MODEL.P,MODEL.B,YEAR,TOT.CPUE.STD,POS.CPUE.STD,PROB.CPUE.STD),NOMI) 
+Best.Mod <- rbind(select(Best.Mod,MODEL_POS,MODEL_PROB,YEAR,CPUE_TOT.STD,CPUE_POS.STD,CPUE_PROB.STD),NOMI) 
 Best.Mod <- melt(Best.Mod,id.var=1:3,variable.name="CPUE_TYPE",value.name="CPUE")
 
 # Best model vs. NOMI graph
-P1 <- ggplot(data=Best.Mod,aes(x=YEAR,y=CPUE,col=str_wrap(MODEL.B,20)))+geom_line()+
-       facet_wrap(~CPUE_TYPE,labeller=labeller(CPUE_TYPE=c("TOT.CPUE.STD"="Combined CPUE","POS.CPUE.STD"="Positive-only CPUE","PROB.CPUE.STD"="Probability CPUE")))+
+P1 <- ggplot(data=Best.Mod,aes(x=YEAR,y=CPUE,col=str_wrap(MODEL_PROB,20)))+geom_line()+
+       facet_wrap(~CPUE_TYPE,labeller=labeller(CPUE_TYPE=c("CPUE_TOT.STD"="Combined CPUE","CPUE_POS.STD"="Positive-only CPUE","CPUE_PROB.STD"="Probability CPUE")))+
        labs(col=paste0("Models (",Ar,")"),linetype=paste0("Models (",Ar,")"))+xlab("Year")+ylab("Standard CPUE (%)")+theme_bw()
 
 #=======================Create trend comparison graphs==============================
-Results.P           <- rbindlist(Results.P)
-Results.P$CPUE_TYPE <- "Positive-only CPUE"
-Results.P           <- select(Results.P,CPUE_TYPE,MODEL=MODEL.P,YEAR,SEASON,AREA_C,WEIGHT,CPUE=POS.CPUE)
-
-Results.B           <- rbindlist(Results.B)
-Results.B$CPUE_TYPE <- "Probability CPUE"
-Results.B           <- select(Results.B,CPUE_TYPE,MODEL=MODEL.B,YEAR,SEASON,AREA_C,WEIGHT,CPUE=PROB.CPUE)
-
-Comp.Mod <- rbind(Results.P,Results.B)
-Comp.Mod <- Comp.Mod[,list(CPUE=sum(CPUE*WEIGHT)),by=list(CPUE_TYPE,MODEL,YEAR,SEASON)] # Sum abundance in all region, by regional weight
+Comp.Mod <- Results[,list(CPUE=sum(CPUE*WEIGHT)),by=list(CPUE_TYPE,MODEL,YEAR,SEASON)] # Sum abundance in all region, by regional weight
 Comp.Mod <- Comp.Mod[,list(CPUE=mean(CPUE)),by=list(CPUE_TYPE,MODEL,YEAR)] # Average all 12 SEASONs per year
 
 Comp.AllYrs       <- Comp.Mod[,list(CPUE.ALLYRS=mean(CPUE)),by=list(CPUE_TYPE,MODEL)] # Average all 12 SEASONs per year
