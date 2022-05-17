6e177a4f0bc374d461b85c6484541ecfb66cda64
Marc Nadon <marc.nadon@noaa.gov>
2022-05-16 15:23:19 -1000

    Update 00_Run all data processing scripts.r

--- Scripts/00_Run all data processing scripts.r
+++ Scripts/00_Run all data processing scripts.r
@@ -11,13 +11,13 @@ dir.create(file.path(root_dir, "Outputs/LBSPR/Graphs"))
 dir.create(file.path(root_dir, "Outputs/LBSPR/Temp size"))
 
 
-source(paste0(this.path::here(.. = 1),"/Scripts/01_CPUE_BBS_Prep.r"));       rm(list=ls()) 
-source(paste0(this.path::here(.. = 1),"/Scripts/02_CPUE_BBS_Wind.r"));       rm(list=ls()) 
-source(paste0(this.path::here(.. = 1),"/Scripts/03_CPUE_BBS_PropTable.r"));  rm(list=ls()) 
-source(paste0(this.path::here(.. = 1),"/Scripts/04_CATCH_BBS_Prep.r"));      rm(list=ls()) 
-source(paste0(this.path::here(.. = 1),"/Scripts/05_CATCH_SBS_PropTable.r")); rm(list=ls()) 
-source(paste0(this.path::here(.. = 1),"/Scripts/06_CATCH_SBS_Prep.r"));      rm(list=ls()) 
-source(paste0(this.path::here(.. = 1),"/Scripts/07_CATCH_FINAL.r"));         rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/01_CPUE_BBS_InitPrep.r"));       rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/02_CPUE_BBS_PropTable.r"));       rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/03a_CPUE_BBS_Wind.r"));  rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/03b_CPUE_BBS_PCA.r"));      rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/04_CPUE_BBS_FinalPrep.r")); rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/05a_CPUE_BBS_Standardize_Function.r"));      rm(list=ls()) 
+source(paste0(this.path::here(.. = 1),"/Scripts/05b_Standardize.r"));         rm(list=ls()) 
 source(paste0(this.path::here(.. = 1),"/Scripts/08_SIZE.r"));                rm(list=ls()) 
 
 
