63fd05371a3e216878cc37e2ca2290022d16ab3e
Marc Nadon <marc.nadon@noaa.gov>
2022-05-18 10:11:17 -1000

    Update 05b_CPUE_BBS_Standardize.r

--- Scripts/05b_CPUE_BBS_Standardize.r
+++ Scripts/05b_CPUE_BBS_Standardize.r
@@ -4,9 +4,11 @@ root_dir <- this.path::here(.. = 1)
 # Load function that fits CPUE models and generate outputs
 source(paste0(root_dir,paste0("/Scripts/05a_CPUE_BBS_Standardize_Function.r")))
 
+#APRU, APVI, CALU, LERU, LUKA, ETCA, ETCO, PRFI, PRFL, PRZO, VALO
 
-Sp <- "LUKA"
-Ar <- c("Tutuila","Manua") [1]
+
+Sp <- "CALU"
+Ar <- c("Tutuila","Manua") [2]
 
 Standardize_CPUE(Sp=Sp,Ar=Ar)
 
