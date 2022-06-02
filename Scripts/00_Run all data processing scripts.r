9e5e6ef2e626dfd1c418f32d7be4eac78ddba78f
Marc Nadon <marc.nadon@noaa.gov>
2022-06-02 09:57:27 -0400

    Clean up 00_f file

--- Scripts/00_Run all data processing scripts.r
+++ Scripts/00_Run all data processing scripts.r
@@ -1,29 +1,22 @@
-require(pacman)
-
 # Check if all required packages are installed, and install if not.
+require(pacman)
 pacman::p_load(boot,data.table,ggfortify,grid,gridExtra,directlabels,mgcv,ncdf4,httr,lubridate,lunar,purrr,googledrive,googlesheets4,RColorBrewer,tidyverse,this.path,viridis)
 
+########## DOWNLOAD DATA FROM GOOGLE DRIVE ###############
 # Check latest data from Google Drive but only download if its more recent than on local repo
 a                  <- drive_ls(path="https://drive.google.com/drive/u/1/folders/1pnH38cupmDU4O_KkKDhYWee_p4sTSD6u", pattern="Data", order_by="recency desc")
 a                  <- a[1,] # Select most recent "Data" zip file
-Date.CurrentFolder <- as_datetime(file.info(paste0(file.path(here(..=1)),"/Data"))$mtime)
-Date.GoogleFolder  <- as_datetime(map_chr(a$drive_resource, "modifiedTime"))
-if(Date.CurrentFolder<Date.GoogleFolder){
-  drive_download(file=a$id, overwrite = TRUE, path = a$name )
-  unzip(a$name)                         }
+if(dir.exists(paste0(file.path(here(..=1)),"/Data"))){
+         Date.CurrentFolder <- as_datetime(file.info(paste0(file.path(here(..=1)),"/Data"))$mtime)
+} else { Date.CurrentFolder <- "1900-01-01 01:01:01 UTC" }
+  
+Date.GoogleFolder <- as_datetime(map_chr(a$drive_resource, "modifiedTime"))
+  if(Date.CurrentFolder<Date.GoogleFolder){
+    drive_download(file=a$id, overwrite = TRUE, path = a$name )
+    unzip(a$name)                         }          
+##########################################################
 
-# Creates outputs folder structure, if necessary
-dir.create(file.path(here(..=1), "Outputs"))
-dir.create(file.path(here(..=1), "Outputs/LBSPR"))
-dir.create(file.path(here(..=1), "Outputs/LBSPR/Graphs"))
-dir.create(file.path(here(..=1), "Outputs/LBSPR/Temp size"))
-dir.create(file.path(here(..=1), "Outputs/SS3_Inputs"))
-dir.create(file.path(here(..=1), "Outputs/SS3_Inputs/CPUE"))
-dir.create(file.path(here(..=1), "Outputs/Summary"))
-dir.create(file.path(here(..=1), "Outputs/Summary/CPUE figures"))
-dir.create(file.path(here(..=1), "Outputs/Summary/Size figures"))
-
-# Run all CATCH and CPUE data processing scripts and export catch files for input into SS
+########## PROCESS CATCH AND CPUE DATA ################
 source(paste0(here(..=1),"/Scripts/01_CPUE_BBS_InitPrep.r"));   rm(list=ls())
 source(paste0(here(..=1),"/Scripts/02_CPUE_BBS_PropTable.r"));  rm(list=ls()) 
 source(paste0(here(..=1),"/Scripts/03a_CPUE_BBS_Wind.r"));      rm(list=ls())
@@ -34,7 +27,9 @@ source(paste0(here(..=1),"/Scripts/07_CATCH_SBS_PropTable.r")); rm(list=ls())
 source(paste0(here(..=1),"/Scripts/08_CATCH_SBS_FinalPrep.r")); rm(list=ls())
 source(paste0(here(..=1),"/Scripts/09_CATCH_Final.r"));         rm(list=ls())
 source(paste0(here(..=1),"/Scripts/10_SIZE.r"));                rm(list=ls()) 
+#######################################################
 
+################ RUN CPUE STANDARDIZATION############################
 # Run CPUE standardization and export indices for input into SS
 source(paste0(here(..=1),"/Scripts/05_CPUE_BBS_Standardize_Function.r"))
 
@@ -50,13 +45,10 @@ for(i in 1:length(Species.List)){
 
 # Or run a single model
 Standardize_CPUE(Sp = "APVI" , Ar = c("Tutuila","Manua") [1])
+#Sp<-"LERU"; Ar<-"Tutuila"; minYr=1988; maxYr=2021
+#####################################################################
 
-
-#Sp<-"LERU"
-#Ar<-"Tutuila" 
-#minYr=1988
-#maxYr=2021
-
+###################### RUN STOCK SYNTHESIS #############################################
 # Script for build_all_ss function
 source(paste0(here(..=1),"/Scripts/11_BUILD_SS3_MODEL.R"))
 
@@ -132,3 +124,5 @@ build_all_ss(species = "APRU",
              runmodels = TRUE,
              ext_args = "-stopph 3 -nohess",
              printreport = FALSE)
+
+###########################################################################################################
