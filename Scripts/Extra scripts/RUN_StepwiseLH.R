4ee63d0e7665d7a951e51a4b010fbeea00f3008e
Marc Nadon <marc.nadon@noaa.gov>
2022-07-18 17:37:56 -1000

    Update RUN_StepwiseLH.R

--- Scripts/Extra scripts/RUN_StepwiseLH.R
+++ Scripts/Extra scripts/RUN_StepwiseLH.R
@@ -1,6 +1,17 @@
-install_github("PIFSCstockassessments/StepwiseLH")
+#install_github("PIFSCstockassessments/StepwiseLH")
 
 require(StepwiseLH); require(ggplot2)
+
+# Get APRU estimates
+# Based on L99 of BBS+Biosampling of 85.8 mm (FL)
+Data <- Get_distributions(Family="Lutjanidae", Lmax.mean=(858/0.85), Lmax.SD=1, M_method="Then_2014",n_iter=3000)
+
+Linf <- median(Data$Linf*0.85)
+Lmat <- median(Data$Lmat*0.85)
+K    <- median(Data$K)
+Amax <- median(Data$Amax)
+M    <- median(Data$M)
+A0   <- median(Data$A0)
 
 
 # Get APVI estimates
@@ -14,23 +25,12 @@ Amax <- median(Data$Amax)
 M    <- median(Data$M)
 A0   <- median(Data$A0)
 
-# Get APRU estimates
-# Based on L99 of BBS 86.5 mm (FL)
-Data <- Get_distributions(Family="Lutjanidae", Lmax.mean=(865/0.85), Lmax.SD=1, M_method="Then_2014",n_iter=3000)
-
-Linf <- median(Data$Linf*0.85)
-Lmat <- median(Data$Lmat*0.85)
-K    <- median(Data$K)
-Amax <- median(Data$Amax)
-M    <- median(Data$M)
-A0   <- median(Data$A0)
-
 
 # Get CALU estimates
 # Based on L99 of 75.8 cm (FL) from BBS
 # Base on L99 of 60.7 cm (FL) from Rose+Swains divers (n=120)
-# Based on L99 of 65.1 cm (FL) from BBS+BIOS
-Data <- Get_distributions(Family="Carangidae", Lmax.mean=(651/0.88), Lmax.SD=1, M_method="Then_2014",n_iter=3000)
+# Based on L99 of 75.2 cm (FL) from BBS+BIOS
+Data <- Get_distributions(Family="Carangidae", Lmax.mean=(752/0.88), Lmax.SD=1, M_method="Then_2014",n_iter=3000)
 
 Linf <- median(Data$Linf*0.88)
 Lmat <- median(Data$Lmat*0.88)
