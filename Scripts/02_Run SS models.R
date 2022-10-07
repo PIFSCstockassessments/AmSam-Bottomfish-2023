1ae983b17da429538315028acef28312b4958c22
Marc Nadon <marc.nadon@noaa.gov>
2022-10-07 12:16:27 -1000

    Update 02_Run SS models.R

--- Scripts/02_Run SS models.R
+++ Scripts/02_Run SS models.R
@@ -5,7 +5,7 @@ Lt     <-vector("list",9) # Species options
 Lt[[1]]<-list("APRU", "SW_Then",         "SW_BBS",          "Kamikawa",    "SW_BBS",   F, c(0.4,1.4), 0.29,    T, list(c(2019,2020)),                           c(1,4,0.2)) 
 Lt[[2]]<-list("APVI", "OMalley_Then",    "OMalley2",        "Biosampling", "Everson",  F, c(0.6,1.6), 0.29,    T, list(c(2004,2006),c(2010,2012)),              c(1,2,0.1)) 
 Lt[[3]]<-list("CALU", "Fry_Then",        "SW_DIV",          "Kamikawa",    "SW_DIV",   F, c(1.3,2.1), 0.29,    T, list(c(2009,2011),c(2016,2017),c(2018,2020)), c(1,4,0.2)) 
-Lt[[4]]<-list("ETCO", "Andrews_Then",    "Andrews_Sex",      "Kamikawa",    "Reed",    F, c(0.5,1.3), 0.29,    T, list(c(2018,2020)),                           c(1,4,0.2)) 
+Lt[[4]]<-list("ETCO", "Andrews_Then",    "Andrews_Sex",     "Kamikawa",    "Reed",     F, c(0.5,1.3), 0.29,    T, list(c(2018,2020)),                           c(1,4,0.2)) 
 Lt[[5]]<-list("LERU", "Loubens_Then",    "Loubens",         "Kamikawa",    "Loubens",  T, c(3.2,4.2), 0.29,    F, NA,                                           c(1,4,0.2)) 
 Lt[[6]]<-list("LUKA", "Morales_Then",    "Loubens2",        "Kamikawa",    "SW_BBS",   T, c(5.4,7.0), 0.23,    F, list(c(2012,2013)),                           c(1,4,0.2)) 
 Lt[[7]]<-list("PRFL", "OMalley_Then",    "OMalley",         "Kamikawa",    "Brouard",  F, c(0.5,1.5), 0.29,    T, list(c(2004,2005),c(2011,2012),c(2018,2020)), c(1,4,0.2)) 
@@ -15,19 +15,17 @@ Lt[[9]]<-list("VALO", "Grandcourt_Then", "Schemmel",        "Kamikawa",    "Sche
 for(i in 1:9){  Lt[[i]]        <- append(Lt[[i]], root_dir)
 names(Lt[[i]]) <- c("N","M","G","LW","MT","IF","R0","Btarg","SY","SY_block","FixedCatchSeq","root")}
 
-#cl    <- makeCluster (5)
-lapply(list(Lt[[1]]),function(x)     { # Run a single model
-  #parLapply(cl,Lt,function(x){ # Run all models
+cl    <- makeCluster (5)
+#lapply(list(Lt[[1]]),function(x)     { # Run a single model
+parLapply(cl,Lt,function(x){ # Run all models
   
-  DirName   <- "34_TestNewReorg"
+  DirName   <- "40_Base"
   runmodels <- F   # Turn off if youy want to process results only
-  N_boot    <- 1   # Set to 0 to turn bootstrap off
-  N_foreyrs <- 8   # Set to 0 to turn forecast off
+  N_boot    <- 0   # Set to 0 to turn bootstrap off
+  N_foreyrs <- 0   # Set to 0 to turn forecast off
   RD        <- F   # Run Diagnostics (jitter, profile, retro)
-  ProfRes   <- 0.2 # R0 profile resolution
+  ProfRes   <- 0.1 # R0 profile resolution
   Begin     <- c(1967,1986)[1]
-  
-  
   
   require(pacman); pacman::p_load(boot,data.table,httr,lubridate,ggpubr,parallel,purrr,googledrive,googlesheets4,gt,quarto,tidyverse,r4ss)
   source(file.path(x$root,"Scripts","02_SS scripts","01_Build_All_SS.R")); source(file.path(x$root,"Scripts","02_SS scripts","06_Run_Diags.R"))
@@ -58,6 +56,7 @@ lapply(list(Lt[[1]]),function(x)     { # Run a single model
     Run_Forecasts(model_dir=file.path(x$root,"SS3 models",x$N,DirName), N_boot=N_boot, N_foreyrs=N_foreyrs, FixedCatchSeq=x$FixedCatchSeq, endyr=2021)
   }    
 })
+
 stopCluster (cl)
 
 
