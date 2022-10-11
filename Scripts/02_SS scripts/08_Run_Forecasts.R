8f3521fb8b36cad253c505b55dff21e232707862
Marc Nadon <marc.nadon@noaa.gov>
2022-10-10 14:52:26 -1000

    Update 08_Run_Forecasts.R

--- Scripts/02_SS scripts/08_Run_Forecasts.R
+++ Scripts/02_SS scripts/08_Run_Forecasts.R
@@ -3,9 +3,9 @@
 #' @param N_foreyrs number of forecast years
 #' @param FixedCatchSeq Sequence of catch containing start, end, and steps of the Fixed Catch values to forecast (ex. start=0, end=1.7 mt, by=0.1)
 
-Run_Forecasts <- function(model_dir, N_boot, N_foreyrs, FixedCatchSeq, endyr){
+Run_Forecasts <- function(model_dir, N_boot, N_foreyrs, FixedCatchSeq, endyr, SavedCores=2){
   
-  require(data.table); require(ggplot2); require(ggpubr); require(tidyverse); require(mgcv)
+  require(data.table);  require(tidyverse)
   
   fore_dir <- file.path(model_dir,"forecast")
   
@@ -18,7 +18,7 @@ Run_Forecasts <- function(model_dir, N_boot, N_foreyrs, FixedCatchSeq, endyr){
   }
   
   # Delete all files in that directory
-  unlink(file.path(fore_dir,"*"))
+  unlink(file.path(fore_dir,"*"),recursive=TRUE)
   
   # Run model one time to generate the data bootstrap files
   file.copy(list.files(model_dir, pattern = "data|control|starter|forecast|.exe", full.names = T),
@@ -58,10 +58,10 @@ Run_Forecasts <- function(model_dir, N_boot, N_foreyrs, FixedCatchSeq, endyr){
       FixedCatchList[[i]] <- list(FixedCatch=FixedCatchVec[i],fore_dir=fore_dir,iboot=iboot,icatch=i)
     }
     
-    n_avail_cores <- detectCores()-1
+    n_avail_cores <- detectCores()-SavedCores
     cl    <- makeCluster (n_avail_cores)
        
-   # Cycke through all catch levels
+   # Cycle through all catch levels
    parLapply(cl,FixedCatchList,function(x){      
         
        require(pacman); pacman::p_load(data.table,tidyverse,r4ss)
@@ -83,16 +83,21 @@ Run_Forecasts <- function(model_dir, N_boot, N_foreyrs, FixedCatchSeq, endyr){
         # Copy the relevant bootstrapped data files to the temp_dir
         file.copy(list.files(fore_dir,pattern=paste0("data_boot_",bootn[iboot],".ss"),full.names=T),to=temp_dir)
         
+        # Obtain bootstrapped catch between 2019-2021 to use for projected catches in 2022 and 2023 (new ACLs starts in 2024)
+        aDat           <- SS_readdat_3.30(file = file.path(temp_dir, paste0("data_boot_", bootn[iboot], ".ss")))
+        aCatch         <- aDat$catch
+        aCatch_Last3yr <- aCatch %>% filter(year>=max(year)-2) %>% summarize(Catch=mean(catch)) %>% as.numeric()
+        
         # Read original forecast file
         forecast.file <- SS_readforecast(file =  file.path(temp_dir, "forecast.ss")) # read forecast file
          
         # Modify the forecast to include a new Fixed Catch to use in the projections
         forecast.file$Nforecastyrs <- N_foreyrs
         forecast.file$ForeCatch    <- data.frame(Year = seq(endyr+1, endyr + N_foreyrs, by = 1),
-                                                                       Season = 1, 
-                                                                       Fleet = 1,
-                                                                       Catch = aFixedCatch)
-        
+                                         Season = 1, 
+                                         Fleet = 1,
+                                         Catch = c(rep(aCatch_Last3yr,times=2),rep(aFixedCatch,times=N_foreyrs-2)))
+     
         # replace forecast file with modified version
         SS_writeforecast(forecast.file, dir = temp_dir, overwrite = TRUE)
         
@@ -126,17 +131,16 @@ Run_Forecasts <- function(model_dir, N_boot, N_foreyrs, FixedCatchSeq, endyr){
   mvlns <- list()
   for(i in 1:length(models)){
     
- #   try(
-    
     aTS                      <- data.table( models[[i]]$timeseries )
-    model.info$FixedCatch[i] <- aTS[Era=="FORE"]$`dead(B):_1`[1]
+    model.info$FixedCatch[i] <- aTS[Era=="FORE"]$`dead(B):_1`[3] # Skip the first 2 years since they are not using the "fixed" catch (i.e. years between model end and start management)
     
+    try(
     mvlns[[i]] <- ss3diags::SSdeltaMVLN(models[[i]], mc = 1000, 
                                         weight = 1, 
                                         run =model.info$model.names[i], 
                                         plot = F,
                                         addprj = T)$kb
-#      , silent=TRUE)
+      , silent=TRUE)
   }
   
   mv                  <- data.table::rbindlist(mvlns)
@@ -149,86 +153,8 @@ Run_Forecasts <- function(model_dir, N_boot, N_foreyrs, FixedCatchSeq, endyr){
   mv_fore             <- select(mv_fore,-c(type,iter,Recr))
   
   # Delete all files and save final result
-   unlink(file.path(fore_dir,"*"))
+   unlink(file.path(fore_dir,"*"),recursive=TRUE)
   
   saveRDS(mv_fore, file = file.path(fore_dir, "mv_projections.rds"))
-  
-#=================Create projection table and figures
-  
- # mv_fore  <- readRDS(file="SS3 models/APVI/33_TestForecast/forecast/mv_projections.rds")
- #  fore_dir  <- file.path(root_dir,"SS3 models","APVI","33_TestForecast","forecast")
-  
-  setnames(mv_fore,"harvest","F_Fmsy") 
-
-  
-  Z  <- mv_fore[Catch==FixedCatch,list(F_Fmsy=median(F_Fmsy),SSB_SSBmsst=median(SSB_SSBmsst)),by=list(year,Catch=FixedCatch)]
-  Z  <- Z[year!=min(year)]
-  
-  P1 <- ggplot(data=Z,aes(x=Catch,y=F_Fmsy,linetype=as.character(year)))+geom_point(size=0.1)+labs(x="Fixed catch (mt)",y="F/Fmsy")+
-          guides(linetype=guide_legend(title="Final year"))+theme_bw()+geom_smooth(col="black",se=F,size=0.5)
-  
-  P2 <- ggplot(data=Z,aes(x=Catch,y=SSB_SSBmsst,linetype=as.character(year)))+geom_point(size=0.1)+labs(x="Fixed catch (mt)",y="SSB/SSBmsst")+
-    theme_bw()+theme(legend.position="none")+geom_smooth(col="black",se=F,size=0.5,span=1)
-  
-  
-  aLegend <- get_legend(P2)
-  ggarrange(P1,P2,ncol=2,common.legend = T,legend.grob = aLegend,legend="right")
-  ggsave(last_plot(),file=file.path(fore_dir,"Catch_MedianStatus.png"),height=8, width=16,units="cm")
-  
-  
-  # Calculate number of iterations by Catch and Year...
-  A <- mv_fore[,list(N_tot=.N),by=list(year,Catch)]
-  
-  # ...That is under overfishing
-  B <- mv_fore[F_Fmsy>1,list(N_overfishing=.N),by=list(year,Catch)]
-  C <- merge(A,B,by=c("year","Catch"),all.x=T)
-  C[is.na(C$N_overfishing)]$N_overfishing <- 0
-  C$ProbOverfishing <- C$N_overfishing/C$N_tot
-  
-  # ...That is overfished
-  D <- mv_fore[SSB_SSBmsst<1,list(N_overfished=.N),by=list(year,Catch)]
-  E <- merge(A,D,by=c("year","Catch"),all.x=T)
-  E[is.na(E$N_overfished)]$N_overfished <- 0
-  E$ProbOverfished <- E$N_overfished/E$N_tot
-  
-  P3 <- ggplot(data=C[year!=min(year)],aes(x=Catch,y=ProbOverfishing,linetype=as.character(year)))+geom_point(size=0.5)+labs(x="Fixed catch (mt)",y="Prob. F > Fmsy")+
-         theme_bw()+theme(legend.position="none")+geom_smooth(col="black",se=F,size=0.5,span=0.3)
-  
-  P4 <- ggplot(data=E[year!=min(year)],aes(x=Catch,y=ProbOverfished,linetype=as.character(year)))+geom_point(size=0.5)+labs(x="Fixed catch (mt)",y="Prob. SSB < SSBmsst")+
-         guides(linetype=guide_legend(title="Final year"))+theme_bw()+geom_smooth(col="black",se=F,size=0.5,span=0.3)
-  
-  aLegend <- get_legend(P4)
-  ggarrange(P3,P4,ncol=2,common.legend = T,legend.grob = aLegend,legend="right")
-  ggsave(last_plot(),file=file.path(fore_dir,"Catch_ProbStatus.png"),height=8, width=16,units="cm")
-  
-  # Catch risk table 
-  G       <- select(C[ProbOverfishing>=0.1&ProbOverfishing<=0.6],-N_tot,-N_overfishing)
-  Preds.x <- expand.grid(ProbOverfishing=seq(0.1,0.5,by=0.01),year=as.factor(seq(min(G$year),max(G$year))))
-  G$year  <- factor(G$year)
-  #model   <- gam(data=G,Catch~year+s(ProbOverfishing,by=year),method="REML")
-  #Preds   <- predict.gam(model,newdata=Preds.x)
-  model   <- glm(data=G,Catch~year*poly(ProbOverfishing,2))
-  Preds   <- predict(model,newdata=Preds.x)
-  Preds   <- cbind(Preds.x,Preds)
-  
-  # Check the GAM model fit and range of data
-  G$year <- as.character(G$year)
-  ggplot()+geom_line(data=Preds,aes(x=Preds,y=ProbOverfishing,col=as.character(year)))+geom_point(data=G,aes(x=Catch,y=ProbOverfishing,col=as.character(year)),shape=2,size=2)+
-         theme_bw()+labs(x="Catch",y="Prob. overfishing")
-  ggsave(last_plot(),file=file.path(fore_dir,"Catch_CheckModelFit.png"),height=8, width=15,units="cm")
-  
-  # Create Prob. overfishing bins
-  G$ProbOverfishing     <- round(G$ProbOverfishing,2)
-  
-  H <- merge(Preds,G,by.x=c("year","ProbOverfishing"),by.y=c("year","ProbOverfishing"),all.x=T)
-  H <- select(H,year,ProbOverfishing,Catch,Preds)
-  
-  # Fill in the blanks
-  H <- H %>% mutate(Catch=coalesce(Catch,Preds)) %>% select(-Preds) %>% 
-           group_by(year,ProbOverfishing) %>% summarize(Catch=round(mean(Catch),2)) %>% 
-             spread(year,Catch) %>% arrange(-ProbOverfishing)
-  
-  write.csv(H,file=file.path(fore_dir,"Catch_Table.csv"),row.names = F)
-    
   
 }
