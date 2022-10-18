3cb2eeb5c32a728521f3b64e46d4960eb3bf5049
Marc Nadon <marc.nadon@noaa.gov>
2022-10-18 10:30:39 -1000

    Update 02_Run SS models.R

--- Scripts/02_Run SS models.R
+++ Scripts/02_Run SS models.R
@@ -4,9 +4,9 @@ Lt     <-vector("list",9) # Species options
 #             Name    M                  Growth             LW             Mat      InitF? R0 prof.  Btarg. SupYer?   SuperYr blocks                        # Projections catch range
 Lt[[1]]<-list("APRU", "SW_Then",         "SW_BBS",          "Kamikawa",    "SW_BBS",   F, c(0.4,1.4), 0.29,    T, list(c(2019,2020)),                           c(1,4,0.2)) 
 Lt[[2]]<-list("APVI", "OMalley_Then",    "OMalley2",        "Biosampling", "Everson",  F, c(0.6,1.6), 0.29,    T, list(c(2004,2006),c(2010,2012)),              c(1.4,3,0.1)) 
-Lt[[3]]<-list("CALU", "Fry_Then",        "SW_DIV",          "Kamikawa",    "SW_DIV",   F, c(1.3,2.1), 0.29,    T, list(c(2009,2011),c(2016,2017),c(2018,2020)), c(1,4,0.2)) 
+Lt[[3]]<-list("CALU", "Fry_Then",        "SW_DIV",          "Kamikawa",    "SW_DIV",   F, c(1.3,2.1), 0.29,    T, list(c(2009,2011),c(2016,2017),c(2018,2020)), c(1,4,0.3)) 
 Lt[[4]]<-list("ETCO", "Andrews_Then",    "Andrews_Sex",     "Kamikawa",    "Reed",     F, c(0.5,1.3), 0.29,    T, list(c(2018,2020)),                           c(1,2.5,0.1)) 
-Lt[[5]]<-list("LERU", "Loubens_Then",    "Loubens",         "Kamikawa",    "Loubens",  T, c(2.6,3.6), 0.29,    F, NA,                                           c(2,4.5,0.2)) 
+Lt[[5]]<-list("LERU", "Loubens_Then",    "Loubens",         "Kamikawa",    "Loubens",  T, c(2.6,3.6), 0.29,    F, NA,                                           c(3,5,0.1)) 
 Lt[[6]]<-list("LUKA", "Morales_Then",    "Loubens2",        "Kamikawa",    "SW_BBS",   T, c(5.4,7.0), 0.23,    F, list(c(2012,2013)),                           c(1,4,0.2)) 
 Lt[[7]]<-list("PRFL", "OMalley_Then",    "OMalley",         "Kamikawa",    "Brouard",  F, c(0.5,1.5), 0.29,    T, list(c(2004,2005),c(2011,2012),c(2018,2020)), c(1,4,0.2)) 
 Lt[[8]]<-list("PRZO", "Schemmel_Then",   "Schemmel_Sex",    "Kamikawa",    "Schemmel", F, c(0.5,1.3), 0.29,    T, list(c(2009,2011),c(2012,2014),c(2015,2016),c(2018,2020)),c(1,4,0.2)) 
@@ -16,7 +16,7 @@ for(i in 1:9){  Lt[[i]]        <- append(Lt[[i]], root_dir)
 names(Lt[[i]]) <- c("N","M","G","LW","MT","IF","R0","Btarg","SY","SY_block","FixedCatchSeq","root")}
 
 #cl    <- makeCluster (5)
-lapply(list(Lt[[5]]),function(x)     { # Run a single model
+lapply(list(Lt[[3]]),function(x)     { # Run a single model
 #parLapply(cl,Lt,function(x){ # Run all models
   
   DirName   <- "50_Base"
@@ -26,7 +26,7 @@ lapply(list(Lt[[5]]),function(x)     { # Run a single model
   RD        <- F  # Run Diagnostics (jitter, profile, retro)
   ProfRes   <- 0.1 # R0 profile resolution
   Begin     <- c(1967,1986)[1]
-  DeleteForecastFiles <- T
+  DeleteForecastFiles <- F
   SavedCores <- 2
   
   require(pacman); pacman::p_load(boot,data.table,httr,lubridate,ggpubr,grid,parallel,purrr,googledrive,googlesheets4,gt,quarto,openxlsx,tidyverse,r4ss)
