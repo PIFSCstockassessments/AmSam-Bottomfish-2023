e2b9bb4738f4579d381476a043ada436ccab3b81
MOshima-PIFSC <megumi.oshima@noaa.gov>
2022-10-31 16:17:02 -1000

    commit

--- Scripts/02_Run SS models.R
+++ Scripts/02_Run SS models.R
@@ -16,13 +16,13 @@ for(i in 1:9){  Lt[[i]]        <- append(Lt[[i]], root_dir)
 names(Lt[[i]]) <- c("N","M","G","LW","MT","IF","R0","Btarg","SY","SY_block","FixedCatchSeq","root")}
 
 #cl    <- makeCluster (5)
-lapply(list(Lt[[3]]),function(x)     { # Run a single model
+lapply(list(Lt[[9]]),function(x)     { # Run a single model
 #parLapply(cl,Lt,function(x){ # Run all models
   
   DirName   <- "50_Base"
   runmodels <- F   # Turn off if you want to process results only
   N_boot    <- 10   # Set to 0 to turn bootstrap off
-  N_foreyrs <- 7   # Set to 0 to turn forecast off
+  N_foreyrs <- 0   # Set to 0 to turn forecast off
   RD        <- F  # Run Diagnostics (jitter, profile, retro)
   ProfRes   <- 0.1 # R0 profile resolution
   Begin     <- c(1967,1986)[1]
@@ -66,7 +66,7 @@ lapply(list(Lt[[3]]),function(x)     { # Run a single model
    }    
 })
 
-stopCluster (cl)
+#stopCluster (cl)
 
 
 
