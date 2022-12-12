96d916f2fe8a741197f580dd1a27ce9ecd4f711f
Marc Nadon <marc.nadon@noaa.gov>
2022-12-11 21:50:23 -1000

    Update 02_Run SS models.R

--- Scripts/02_Run SS models.R
+++ Scripts/02_Run SS models.R
@@ -6,21 +6,21 @@ Lt[[1]]<-list("APRU", "SW_Then",         "SW_BBS_BIOS",     "Kamikawa",    "SW_B
 Lt[[2]]<-list("APVI", "OMalley_Then",    "OMalley2",        "Kamikawa",    "Everson",     F, c(0.6,1.6), 0.29,    T, list(c(2004,2006),c(2010,2012)),             c(1.4,3,0.1)) 
 Lt[[3]]<-list("CALU", "Fry_Then",        "SW_BBS_BIOS",     "Kamikawa",    "SW_BBS_BIOS", F, c(0.8,1.8), 0.29,    T, list(c(2009,2011),c(2016,2017),c(2018,2020)),c(0.8,2.0,0.1)) 
 Lt[[4]]<-list("ETCO", "Andrews_Then",    "Andrews_Sex",     "Kamikawa",    "Reed",        F, c(0.5,1.3), 0.29,    T, list(c(2018,2020)),                          c(1,2.5,0.1)) 
-Lt[[5]]<-list("LERU", "Loubens_Then",    "Loubens",         "Kamikawa",    "Loubens",     T, c(2.6,3.6), 0.29,    F, NA,                                          c(3,5,0.1)) 
+Lt[[5]]<-list("LERU", "Loubens_Then",    "Loubens",        "Kamikawa",    "Loubens",     T, c(2.6,3.6), 0.29,    F, NA,                                          c(3,5,0.1)) 
 Lt[[6]]<-list("LUKA", "Loubens_Then",    "Loubens2",        "Kamikawa",    "SW_BBS_BIOS", T, c(5.4,7.0), 0.23,    F, NA,                                          c(20,35,1)) 
 Lt[[7]]<-list("PRFL", "OMalley_Then",    "OMalley",         "Kamikawa",    "SW_BBS_BIOS", F, c(0.5,1.5), 0.29,    T, list(c(2004,2005),c(2011,2012),c(2018,2020)),c(0.5,1.5,0.1)) 
 Lt[[8]]<-list("PRZO", "Schemmel_Then",   "Schemmel_Sex",    "Kamikawa",    "Schemmel",    F, c(0.5,1.3), 0.29,    T, list(c(2009,2011),c(2012,2014),c(2015,2016), c(2018,2020)),c(0.5,1.0,0.05)) 
-Lt[[9]]<-list("VALO", "Grandcourt_Then", "SW_BBS_BIOS",     "Kamikawa",    "Schemmel",    F, c(1.0,2.4),  0.35,   T, list(c(2016,2020)),                          c(0.6,1.4,0.1)) 
+Lt[[9]]<-list("VALO", "Grandcourt_Then", "SW_BBS_BIOS",     "Kamikawa",    "Schemmel",    F, c(1.0,2.4), 0.35,    T, list(c(2016,2020)),                          c(0.6,1.4,0.1)) 
 
 for(i in 1:9){  Lt[[i]]        <- append(Lt[[i]], root_dir)
 names(Lt[[i]]) <- c("N","M","G","LW","MT","IF","R0","Btarg","SY","SY_block","FixedCatchSeq","root")}
 
-#cl    <- makeCluster (5)
-lapply(list(Lt[[7]]),function(x)     { # Run a single model
+cl    <- makeCluster (9)
+lapply(list(Lt[[5]]),function(x)     { # Run a single model
 #parLapply(cl,Lt,function(x){ # Run all models
   
-  DirName   <- "102_NoHistCatchTEST"
-  runmodels <- F   # Turn off if you want to process results only
+  DirName   <- "102_AlternatelH"
+  runmodels <- T   # Turn off if you want to process results only
   N_boot    <- 0   # Set to 0 to turn bootstrap off
   N_foreyrs <- 0   # Set to 0 to turn forecast off
   RD        <- F  # Run Diagnostics (jitter, profile, retro)
@@ -28,7 +28,7 @@ lapply(list(Lt[[7]]),function(x)     { # Run a single model
   Begin     <- c(1967,1986)[1]
   DeleteForecastFiles <- F
   SavedCores <- 2
-  Create_species_report_figs <- T
+  Create_species_report_figs <- F
   
   require(pacman); pacman::p_load(boot,data.table,httr,lubridate,ggpubr,grid,parallel,purrr,googledrive,googlesheets4,gt,quarto,openxlsx,tidyverse,r4ss,officer,flextable)
   source(file.path(x$root,"Scripts","02_SS scripts","01_Build_All_SS.R")); source(file.path(x$root,"Scripts","02_SS scripts","06_Run_Diags.R"))
@@ -70,7 +70,7 @@ lapply(list(Lt[[7]]),function(x)     { # Run a single model
   
   if(Create_species_report_figs){
     
-    file.copy(from = file.path(root_dir,"Scripts","03_Report scripts",
+    file.copy(from = file.path(X$root,"Scripts","03_Report scripts",
                                "Create_Figs_Tables_Formatted.qmd"), 
               to = file.path(model_dir, 
                              "Create_Figs_Tables_Formatted.qmd"),
