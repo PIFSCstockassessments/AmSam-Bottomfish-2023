ac8a0432c01bfc2b75a8125b6836f530e847c048
Marc Nadon <marc.nadon@noaa.gov>
2022-12-15 08:27:30 -1000

    Update Run_VB fits.R

--- Scripts/04_Other scripts/Run_VB fits.R
+++ Scripts/04_Other scripts/Run_VB fits.R
@@ -11,9 +11,18 @@ D <- data.table( read.xlsx(file.path(dir,"Data","ETCO_MHI preliminary length at 
 D <- select(D,SEX,LENGTH_FL=LENGTH_CM,AGE=Final.Age)
 ggplot(data=D,aes(x=AGE,y=LENGTH_FL,col=SEX))+geom_point()
 
+
+# VALO data
+D <- data.table( read.csv(file.path(dir,"Data","VALO LH data.csv")) )
+ggplot(data=D,aes(x=AGE,y=LENGTH_FL))+geom_point()
+
+
 BreakAge <- 0
 
+
 VB <- function(VBpar,x){ VBpar[1]*(1-exp(-VBpar[2]*(x-VBpar[3]))) }
+VB.T0 <- function(VBpar,x){ VBpar[1]*(1-exp(-VBpar[2]*(x))) }
+
 VB2_Fit <- function(param,BreakAge,x){
   
   Linf<-param[1]; K<-param[2]; a0<-param[3]; Slope<-param[4]; SD_Yg<-param[5]; CV_Old<-param[6]
@@ -31,9 +40,10 @@ VB2_Fit <- function(param,BreakAge,x){
   
 }
 
-
+R0 <- nls(LENGTH_FL~Linf*(1-exp(-K*(AGE))),data=D,start=list(Linf=70,K=0.4))
 R1 <- nls(LENGTH_FL~Linf*(1-exp(-K*(AGE-a0))),data=D,start=list(Linf=70,K=0.4,a0=0))
 R2 <- nlm(VB2_Fit,p=c(80,0.15,-5,16,5,0.1),BreakAge=BreakAge,x=D)
+
 
 #coef(R1)
 R2$estimate
@@ -42,8 +52,11 @@ E                         <- data.table(AGE=seq(from=0,to=max(D$AGE),by=0.5))
 E$LENGTH_FL               <- VB(R2$estimate[1:3],E$AGE)
 E[AGE<BreakAge]$LENGTH_FL <- E[AGE<BreakAge]$AGE*R2$estimate[4]
 
-ggplot()+geom_point(data=D,aes(x=AGE,y=LENGTH_FL))+geom_line(data=E,aes(x=AGE,y=LENGTH_FL),col="red",size=1)+
-  geom_line(aes(x=E$AGE,y=VB(coef(R1),E$AGE)),col="blue",size=1)
+ggplot()+geom_point(data=D,aes(x=AGE,y=LENGTH_FL))+
+  geom_line(data=E,aes(x=AGE,y=LENGTH_FL),col="red",size=1)+
+  geom_line(aes(x=E$AGE,y=VB(coef(R1),E$AGE)),col="blue",size=1)+
+  geom_line(aes(x=E$AGE,y=VB.T0(coef(R0),E$AGE)),col="orange",size=1)+
+  scale_y_continuous(expand=c(0,0),limits=c(0,NA))
 
 VB(R2$estimate[1:3],3)
 
