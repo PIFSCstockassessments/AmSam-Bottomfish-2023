b7340dad68400e998da711f79dcdb5d0cd45a568
Marc Nadon <marc.nadon@noaa.gov>
2022-12-29 14:59:13 -1000

    Update Run_VB fits.R

--- Scripts/04_Other scripts/Run_VB fits.R
+++ Scripts/04_Other scripts/Run_VB fits.R
@@ -3,21 +3,21 @@ require(data.table); require(this.path); require(openxlsx); require(ggplot2); re
 dir <- this.path::here(.. = 2)
 
 # APVI data
-#D <- data.table( read.xlsx(file.path(dir,"Data","APVI_EVB for SAP.xlsx")) )
-#D <- select(D,REGION=location,LENGTH_FL=length,AGE=age)
+D <- data.table( read.xlsx(file.path(dir,"Data","APVI_EVB for SAP.xlsx")) )
+D <- select(D,REGION=location,LENGTH_FL=length,AGE=age)
 
 # ETCO data
-D <- data.table( read.xlsx(file.path(dir,"Data","ETCO_MHI preliminary length at age 2022.xlsx")) )
-D <- select(D,SEX,LENGTH_FL=LENGTH_CM,AGE=Final.Age)
-ggplot(data=D,aes(x=AGE,y=LENGTH_FL,col=SEX))+geom_point()
+#D <- data.table( read.xlsx(file.path(dir,"Data","ETCO_MHI preliminary length at age 2022.xlsx")) )
+#D <- select(D,SEX,LENGTH_FL=LENGTH_CM,AGE=Final.Age)
+#ggplot(data=D,aes(x=AGE,y=LENGTH_FL,col=SEX))+geom_point()
 
 
 # VALO data
-D <- data.table( read.csv(file.path(dir,"Data","VALO LH data.csv")) )
-ggplot(data=D,aes(x=AGE,y=LENGTH_FL))+geom_point()
+#D <- data.table( read.csv(file.path(dir,"Data","VALO LH data.csv")) )
+#ggplot(data=D,aes(x=AGE,y=LENGTH_FL))+geom_point()
 
 
-BreakAge <- 0
+BreakAge <- 3
 
 
 VB <- function(VBpar,x){ VBpar[1]*(1-exp(-VBpar[2]*(x-VBpar[3]))) }
@@ -42,7 +42,8 @@ VB2_Fit <- function(param,BreakAge,x){
 
 R0 <- nls(LENGTH_FL~Linf*(1-exp(-K*(AGE))),data=D,start=list(Linf=70,K=0.4))
 R1 <- nls(LENGTH_FL~Linf*(1-exp(-K*(AGE-a0))),data=D,start=list(Linf=70,K=0.4,a0=0))
-R2 <- nlm(VB2_Fit,p=c(80,0.15,-5,16,5,0.1),BreakAge=BreakAge,x=D)
+R1.NoYoung <- nls(LENGTH_FL~Linf*(1-exp(-K*(AGE-a0))),data=D[AGE>=BreakAge],start=list(Linf=76,K=0.1,a0=-2))
+R2 <- nlm(VB2_Fit,p=c(76.9,0.13,-2,10,4.6,0.1),BreakAge=BreakAge,x=D)
 
 
 #coef(R1)
